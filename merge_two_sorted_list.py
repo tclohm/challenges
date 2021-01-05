d45f065c4cfe58a217b1f189e893f3af52e22b8c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2021-01-05 08:55:27 -0800

    merge_two_sorted_list altered

--- merge_two_sorted_list.py
+++ merge_two_sorted_list.py
@@ -13,44 +13,61 @@ def mergeTwoLists(list1: ListNode, list2: ListNode) -> ListNode:
 	# check to see who has the the lower value in list1 or list2
 	# create new node List and place the lower value into the node
 	# move to new node and point to next
-	head = ListNode(None)
-	current_node = head
-	pointer_one = list1
-	pointer_two = list2
+	# head = ListNode(None)
+	# current_node = head
+	# pointer_one = list1
+	# pointer_two = list2
 
-	while pointer_one is not None and pointer_two is not None:
+	# while pointer_one is not None and pointer_two is not None:
 
-		if pointer_one.value is None:
-			print("pointer 1 value is none")
-			new_node = ListNode(pointer_two.value)
-			current_node.next = new_node
-			current_node = current_node.next
-			pointer_two = pointer_two.next
+	# 	if pointer_one.value is None:
+	# 		print("pointer 1 value is none")
+	# 		new_node = ListNode(pointer_two.value)
+	# 		current_node.next = new_node
+	# 		current_node = current_node.next
+	# 		pointer_two = pointer_two.next
 
-		elif pointer_two.value is None:
-			print("pointer 2 value is none")
-			new_node = ListNode(pointer_one.value)
-			current_node.next = new_node
-			current_node = current_node.next
-			pointer_one= pointer_one.next
+	# 	elif pointer_two.value is None:
+	# 		print("pointer 2 value is none")
+	# 		new_node = ListNode(pointer_one.value)
+	# 		current_node.next = new_node
+	# 		current_node = current_node.next
+	# 		pointer_one= pointer_one.next
 
-		elif pointer_one.value <= pointer_two.value:
-			new_node = ListNode(pointer_one.value)
-			current_node.next = new_node
-			current_node = current_node.next
-			pointer_one = pointer_one.next
-			print("pointer 1 is injected", current_node)
-			print("pointer 1 next is ", pointer_one)
+	# 	elif pointer_one.value <= pointer_two.value:
+	# 		new_node = ListNode(pointer_one.value)
+	# 		current_node.next = new_node
+	# 		current_node = current_node.next
+	# 		pointer_one = pointer_one.next
+	# 		print("pointer 1 is injected", current_node)
+	# 		print("pointer 1 next is ", pointer_one)
 
-		elif pointer_one.value > pointer_two.value:
-			new_node = ListNode(pointer_two.value)
-			current_node.next = new_node
-			current_node = current_node.next
-			pointer_two = pointer_two.next
-			print("pointer two is injected", current_node)
-			print("pointer 2 next is", pointer_two)
+	# 	elif pointer_one.value > pointer_two.value:
+	# 		new_node = ListNode(pointer_two.value)
+	# 		current_node.next = new_node
+	# 		current_node = current_node.next
+	# 		pointer_two = pointer_two.next
+	# 		print("pointer two is injected", current_node)
+	# 		print("pointer 2 next is", pointer_two)
+
+	# return head.next
+	head = result = ListNode(None)
+	while list1 and list2:
+
+		if list1.value <= list2.value:
+			result.next = ListNode(list1.value)
+			list1 = list1.next
+		else:
+			result.next = ListNode(list2.value)
+			list2 = list2.next
+
+		result = result.next
+
+	tail = list1 or list2
+	result.next = tail
 
 	return head.next
+
 
 
 if __name__ == "__main__":
