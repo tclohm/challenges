8da6a12e60024d9790c5b8d0c5d4bbb1bad8b55c
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-08-11 16:01:08 -0700

    playing with pointers to mergeTwoLinkedLists

--- codesig/mergeTwoLinkedLists.go
+++ codesig/mergeTwoLinkedLists.go
@@ -1,57 +1,53 @@
+// Singly-linked lists are already defined with this interface:
+// type ListNode struct {
+//   Value interface{}
+//   Next *ListNode
+// }
+//
+func exhaust(linkedlist **ListNode, into **ListNode) {
+    for *linkedlist != nil {
+        (*into).Value = (*linkedlist).Value
+        
+        if (*linkedlist).Next != nil {
+            (*into).Next = &ListNode{}
+        }
+
+        *linkedlist = (*linkedlist).Next
+        *into = (*into).Next      
+    }
+    return
+}
+
+func mergeAndNext(linkedlist **ListNode, into **ListNode) {
+    (*into).Value = (*linkedlist).Value
+    *into = (*into).Next
+    *linkedlist = (*linkedlist).Next
+    return
+}
+
 func mergeTwoLinkedLists(l1 *ListNode, l2 *ListNode) *ListNode {
     result := &ListNode{}
     head := result
-    
     if l1 == nil && l2 == nil {
         return l1
     }
-    
     for l2 != nil || l1 != nil {
-        
-        if l1 != nil || l2 != nil {
-            result.Next = &ListNode{}
-        }
-        
         if l1 != nil && l2 != nil {
             if l1.Value.(int) <= l2.Value.(int) {
-                result.Value = l1.Value
-                result = result.Next
-                l1 = l1.Next
+                result.Next = &ListNode{}
+                mergeAndNext(&l1, &result)
             } else {
-                result.Value = l2.Value
-                result = result.Next
-                l2 = l2.Next
+                result.Next = &ListNode{}
+                mergeAndNext(&l2, &result)
             }
         }
         
         if l1 == nil && l2 != nil {
-            for l2 != nil {
-                
-                result.Value = l2.Value
-                
-                if l2.Next != nil {
-                    result.Next = &ListNode{}
-                }
-
-                l2 = l2.Next
-                result = result.Next
-                
-            }
+            exhaust(&l2, &result)
         } 
         
         if l1 != nil && l2 == nil {
-            for l1 != nil {
-                
-                result.Value = l1.Value
-                
-                if l1.Next != nil {
-                    result.Next = &ListNode{}
-                }
-
-                l1 = l1.Next
-                result = result.Next
-                
-            }
+            exhaust(&l1, &result)
         }
     }
     
@@ -74,5 +70,28 @@ func mergeTwoLinkedLists(l1 *ListNode, l2 *ListNode) *ListNode {
     } else {
         l2.Next = mergeTwoLinkedLists(l1, l2.Next)
         return l2
+    }
+}
+
+
+func mergeTwoLinkedLists(l1 *ListNode, l2 *ListNode) *ListNode {
+    res := &ListNode{}
+    r := res
+    for l1 != nil || l2 != nil {
+        r.Next = &ListNode{min(&l1, &l2), nil}
+        r = r.Next
+    }
+    return res.Next
+}
+
+func min(l1 **ListNode, l2 **ListNode) (minVal int) {
+    if *l1 == nil || (*l2 != nil && (*l1).Value.(int) > (*l2).Value.(int)) {
+        minVal = (*l2).Value.(int)
+        *l2 = (*l2).Next
+        return
+    } else {
+        minVal = (*l1).Value.(int)
+        *l1 = (*l1).Next
+        return
     }
 }
