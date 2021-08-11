3c64efc467db24b8dce40904904496a575bbc1dc
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-08-11 15:18:01 -0700

    recursive mergeTwoLinkedLists

--- codesig/mergeTwoLinkedLists.go
+++ codesig/mergeTwoLinkedLists.go
@@ -57,3 +57,22 @@ func mergeTwoLinkedLists(l1 *ListNode, l2 *ListNode) *ListNode {
     
     return head
 }
+
+
+func mergeTwoLinkedLists(l1 *ListNode, l2 *ListNode) *ListNode {
+    if l1 == nil {
+        return l2
+    }
+    
+    if l2 == nil {
+        return l1
+    }
+    
+    if l1.Value.(int) <= l2.Value.(int) {
+        l1.Next = mergeTwoLinkedLists(l1.Next, l2)
+        return l1
+    } else {
+        l2.Next = mergeTwoLinkedLists(l1, l2.Next)
+        return l2
+    }
+}
