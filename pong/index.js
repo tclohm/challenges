f2c58582f7ed17103d8d6063b65731f1c4f6ab02
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-08-31 16:27:59 -0700

    collisions into paddle are wonking

--- pong/index.js
+++ pong/index.js
@@ -9,6 +9,47 @@ const puck = new Puck(400, 400);
 const width = 401;
 const height = 401;
 
+const left = new Paddle(0+10);
+const right = new Paddle(width-20)
+
+document.addEventListener('keyup', function(input) {
+	if (input.keyCode === 87) {
+			// MARK: -- up
+			left.move(0)
+		}
+		if (input.keyCode === 83) {
+			// MARK: -- down
+			left.move(0)
+		}
+		if (input.keyCode === 38) {
+			// MARK: -- up
+			right.move(0)
+		}
+		if (input.keyCode === 40) {
+			// MARK: -- down
+			right.move(0)
+		}
+})
+
+document.addEventListener('keydown', function(input) {
+		if (input.keyCode === 87) {
+			// MARK: -- up 
+			left.move(-5)
+		}
+		if (input.keyCode === 83) {
+			// MARK: -- down
+			left.move(5)
+		}
+		if (input.keyCode === 38) {
+			// MARK: -- up
+			right.move(-5)
+		}
+		if (input.keyCode === 40) {
+			// MARK: -- down
+			right.move(5)
+		}
+})
+
 function setup() {
 	createCanvas(width, height, 'rgba(0, 0, 0, 1)');
 }
@@ -17,6 +58,16 @@ function draw() {
 	const ctx = document.getElementById('canvas').getContext('2d');
 	ctx.clearRect(0, 0, 800, 800);
 	setup()
+
+	puck.checkPaddleRight(right);
+	puck.checkPaddleLeft(left);
+
+	left.show()
+	right.show()
+
+	left.update()
+	right.update()
+
 	puck.update()
 	puck.edges(height, width)
 	puck.show()
@@ -24,7 +75,7 @@ function draw() {
 
 function main() {
 	setup()
-	setInterval(draw, 20)
+	setInterval(draw, 10)
 }
 
 main()
