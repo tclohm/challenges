80705299b4c5edf7c2383dd24c9e3d608e3ae3a8
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-09-05 12:09:45 -0700

    done circle packing for now

--- circle/circle.js
+++ circle/circle.js
@@ -2,12 +2,12 @@ class Circle {
 	constructor(x_, y_) {
 		this.x = x_;
 		this.y = y_;
-		this.r = 40;
+		this.r = 1;
+		this.growing = true;
 	}
 
 	show() {
 		const ctx = document.getElementById('canvas').getContext('2d');
-		ctx.beginPath();
 		ctx.strokeStyle = 'white';
 		ctx.lineWidth = 2;
 		ctx.ellipse(this.x, this.y, this.r, this.r, 0, 0, 2 * Math.PI);
@@ -15,6 +15,12 @@ class Circle {
 	}
 
 	grow() {
-		this.r = this.r + 1;
+		if (this.growing) {
+			this.r += 1
+		}
+	}
+
+	edges() {
+		return this.x + this.r > width || this.x - this.r < 0 || this.y + this.r > height || this.y - this.r < 0;
 	}
 }
