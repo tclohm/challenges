01e4561b189260888327c44f211ab0556f5f8a83
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-09-08 14:55:08 -0700

    done with quadtree for now

--- quadtree/index.js
+++ quadtree/index.js
@@ -2,10 +2,10 @@ const width = 800;
 const height = 800;
 function setup() {
 	size(width, height, 'rgba(0,0,0,0.8)');
-	let boundary = new Rectangle(200, 200, 200, 200);
+	let boundary = new Rectangle(400, 400, 200, 200);
 	let qt = new QuadTree(boundary, 4);
 
-	for (let i = 0 ; i < 50 ; i++) {
+	for (let i = 0 ; i < 500 ; i++) {
 		const p = new Point(Math.floor(Math.random() * width), Math.floor(Math.random() * height))
 		qt.insert(p)
 	}
