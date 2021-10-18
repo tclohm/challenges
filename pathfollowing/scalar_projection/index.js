9b61beabaa47287b4131ec9d60048e82daffc50b
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-10-18 07:16:39 -0700

    scalars are cool"

--- pathfollowing/scalar_projection/index.js
+++ pathfollowing/scalar_projection/index.js
@@ -3,7 +3,11 @@ let a, b;
 function setup() {
 	size(400, 400, 'rgba(0,0,0,0.8)')
 
-	a = createVector(100, -60)
+	const canvas = document.getElementById('canvas')
+	canvas.addEventListener('mousemove', (e) => {
+		a = createVector(e.x, e.y)
+	})
+
 	b = createVector(200, 60)
 }
 
@@ -15,9 +19,12 @@ function scalarProjection(a,b) {
 }
 
 function draw() {
+	setup()
 	const ctx = document.getElementById('canvas').getContext('2d');
 	const pos = createVector(100, 200)
 	ctx.strokeStyle = "white"
+
+	
 	line(pos.x, pos.y, pos.x + a.x, pos.y + a.y)
 	line(pos.x, pos.y, pos.x + b.x, pos.y + b.y)
 
@@ -26,7 +33,20 @@ function draw() {
 	const v = scalarProjection(a, b)
 	ctx.strokeStyle = "blue"
 	line(pos.x, pos.y, pos.x + v.x, pos.y + v.y)
+
+	ctx.strokeStyle = "green"
+	line(pos.x + a.x, pos.y + a.y, v.x + pos.x, v.y + pos.y);
+
+	circle(v.x + pos.x, v.y + pos.y, 8)
+
+	circle(pos.x + a.x, pos.y + a.y, 8)
+
 }
 
-setup()
-draw()
+function main() {
+	setup()
+	setInterval(draw, 10)
+}
+
+main()
+
