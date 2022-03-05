9af0283d1ea5e6ca76ab44377e973b313168de00
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2022-03-04 16:13:01 -0800

    added four different ways

--- brad/anagram_detection/anagram_detection.go
+++ brad/anagram_detection/anagram_detection.go
@@ -4,8 +4,10 @@ import (
 	"strings"
 	"fmt"
 	"sort"
+	"reflect"
 )
 
+// MARK: -- O(n^2)
 func anagram_checking_off(string1, string2 string) bool {
 	if len(string1) != len(string2) {
 		return false
@@ -31,6 +33,7 @@ func anagram_checking_off(string1, string2 string) bool {
 	return true
 }
 
+// MARK: -- O(n log n)
 func anagram_sort_and_compare(s1, s2 string) bool {
 	if len(s1) != len(s2) {
 		return false
@@ -53,6 +56,7 @@ func anagram_sort_and_compare(s1, s2 string) bool {
 	return true
 }
 
+// MARK: -- O(n)
 func anagram_count_compare(s1, s2 string) bool {
 	if len(s1) != len(s2) {
 		return false
@@ -79,6 +83,33 @@ func anagram_count_compare(s1, s2 string) bool {
 	return true
 }
 
+// MARK: -- O(n)
+func anagram_with_counter(s1, s2 string) bool {
+	var dict1 = make(map[string]int)
+	var dict2 = make(map[string]int)
+
+	for i := 0 ; i < len(s1) ; i++ {
+		char := string(s1[i])
+		if _, ok := dict1[char]; ok {
+			dict1[char]++
+		} else {
+			dict1[char] = 1
+		}
+		
+	}
+
+	for i := 0 ; i < len(s2) ; i++ {
+		char := string(s2[i])
+		if _, ok := dict2[char]; ok {
+			dict2[char]++
+		} else {
+			dict2[char] = 1
+		}
+	}
+	
+	return reflect.DeepEqual(dict1, dict2)
+}
+
 func main() {
 	fmt.Println(anagram_checking_off("abcd", "dcba"))
 	fmt.Println(anagram_checking_off("abcd", "abcc"))
@@ -89,4 +120,7 @@ func main() {
 	fmt.Println("")
 	fmt.Println(anagram_count_compare("apple", "pleap"), "( apple, pleap )")
 	fmt.Println(anagram_count_compare("apple", "applf"), "( apple, applf )")
+	fmt.Println("---")
+	fmt.Println(anagram_with_counter("apple", "pleap"), "( apple, pleap )")
+	fmt.Println(anagram_with_counter("apple", "applf"), "( apple, applf )")
 }
