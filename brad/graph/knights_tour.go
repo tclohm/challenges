bdfa6255ac8a06e47cf758178f94b890025b4498
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2022-03-16 16:08:13 -0700

    shrug

--- brad/graph/knights_tour.go
+++ brad/graph/knights_tour.go
@@ -22,6 +22,35 @@ func add_edge(g Graph, node1, node2 Set) {
 	g[node2] = append(g[node2], node1)
 }
 
+func build_graph(board_size int) Graph {
+	graph := Graph{}
+
+	for row := 0 ; row < len(board_size) ; row++ {
+		for col := 0 ; col < len(board_size) ; col++ {
+
+		}
+	}
+}
+
+func legal_moves_from(row int, col int, board_size int) (int, int)  {
+	MOVE_OFFSETS := [][]int{
+				{-1, -2}, {1, -2},
+		{-2, -1},				{2, -1},
+		{-2, 1},				{2, 1},
+				{-1, 2}, {1, 2},
+	}
+
+	for _, offset := range MOVE_OFFSETS {
+		row_offset, col_offset := offset[0], offset[1]
+		move_row, move_col := row + row_offset, col + col_offset
+		
+		if 0 <= move_col && move_col < board_size  && 0 <= move_row && move_col < board_size {
+			return move_row, move_col
+		}
+	}
+
+	retu
+
 func main() {
 	g := Graph{}
 	fmt.Println(g)
