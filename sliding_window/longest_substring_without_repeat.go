56c3ab7c6f261da3d763aa3ab635f036d66714da
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-06-28 21:40:17 -0700

    lswrc

--- sliding_window/longest_substring_without_repeat.go
+++ sliding_window/longest_substring_without_repeat.go
@@ -2,22 +2,47 @@ package main
 
 import "fmt"
 
-func length_of_longest_substring(s string) string {
-	var left, right int = 0, 1
-	var longest string = ""
-
-	for right < len(s) {
-		if s[left] == s[right] {
-			left = right
-		} else {
-
-		}
-		right += 1
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
 	}
+}
+
+func does(r rune, existIn []rune) bool {
+	for _, char := range existIn {
+		if r == char {
+			return true
+		}
+	}
+	return false
+}
+
+func length_of_longest_substring(s string) int {
+	var set = []rune{}
+	var left int = 0
+	
+	var result int = 0
+
+	for right, char := range s {
+		for does(char, set) {
+				length := len(set)
+				set = set[1:length]
+				left += 1
+		}
+		result = max(result, right - left + 1)
+		set = append(set, char)
+	}
+	return result
 }
 
 func main() {
 	s1 := "abcabcbb"
 	s2 := "bbbbb"
 	s3 := "pwwkew"
+
+	fmt.Println(length_of_longest_substring(s1))
+	fmt.Println(length_of_longest_substring(s2))
+	fmt.Println(length_of_longest_substring(s3))
 }
