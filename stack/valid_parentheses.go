30eade6b02187189e40a593a5e58084d3b35fe3c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-06-29 15:31:24 -0700

    valid stack other way

--- stack/valid_parentheses.go
+++ stack/valid_parentheses.go
@@ -48,6 +48,47 @@ func isValid(s string) bool {
 	return len(stack) == 0
 }
 
+type stack []int32
+func (s *stack) Push(v int32) {
+	*s = append(*s, v)
+}
+func (s *stack) Pop() {
+	if s.IsEmpty() {
+		return
+	}
+	*s =  (*s)[len(*s) - 1]
+}
+func (s *stack) Top() int32 {
+	if s.IsEmpty() {
+		return 0
+	}
+	return (*s)[len(*s) - 1]
+}
+func (s *stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+func validity(s string) bool {
+	stck := make(stack, 0)
+	for _, v := range s {
+		switch {
+		case v == '(':
+			stck.Push(')')
+		case v == '{':
+			stck.Push('}')
+		case v == '[':
+			stck.Push(']')
+		default:
+			if stck.IsEmpty() || v != stck.Top() {
+				return false
+			} else {
+				stck.Pop()
+			}
+		}
+	}
+	return stck.IsEmpty()
+}
+
 func main() {
 	v := "()"
 	vv := "()[]{}"
