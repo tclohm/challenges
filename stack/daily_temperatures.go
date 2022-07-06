97e03ec81bb9ebef38beedd3986f67d55ed1a003
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-07-06 07:45:00 -0700

    daily temp

--- stack/daily_temperatures.go
+++ stack/daily_temperatures.go
@@ -3,11 +3,33 @@ package main
 import "fmt"
 
 func daily(temps []int) []int {
-    
+    length := len(temps)
+    hottest := 0
+    answer := make([]int, length, length)
+
+    for currentDay := length - 1 ; currentDay > 0 ; currentDay-- {
+    	currentTemp := temps[currentDay]
+    	if currentTemp >= hottest {
+    		hottest = currentTemp
+    		continue
+    	}
+
+    	days := 1
+
+    	for temps[currentDay + days] <= currentTemp {
+    		days += answer[currentDay + days]
+    	}
+    	answer[currentDay] = days
+    }
+    return answer
 }
 
 func main() {
 	t1 := []int{73,74,75,71,69,72,76,73}
 	t2 := []int{30,40,50,60}
 	t3 := []int{30,60,90}
+
+	fmt.Println(daily(t1))
+	fmt.Println(daily(t2))
+	fmt.Println(daily(t3))
 }
