feddad0a9f97a8c950940524ba6eaacca238176b
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-07-05 21:41:07 -0700

    daily temp

---
+++ stack/daily_temperatures.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func daily(temps []int) []int {
+    
+}
+
+func main() {
+	t1 := []int{73,74,75,71,69,72,76,73}
+	t2 := []int{30,40,50,60}
+	t3 := []int{30,60,90}
+}
