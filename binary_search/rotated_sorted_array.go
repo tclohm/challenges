2c837d61985ee38f957217300344c0256d853b1f
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-07-09 21:18:40 -0700

    bs rotated

--- binary_search/rotated_sorted_array.go
+++ binary_search/rotated_sorted_array.go
@@ -11,15 +11,48 @@ func bruteForceSearch(nums []int, target int) int {
 	return -1
 }
 
+func binarySearch(nums []int, target int) int {
+	// left sorted portion
+	// right sorted portion
+	var left = 0
+	var right = len(nums) - 1
+
+	for left <= right {
+		// find middle
+		middle := left + (right - left) / 2
+		// if the middle the target return the target
+		if nums[middle] == target {
+			return middle
+		}
+		// if middle is less than left
+		if nums[left] <= nums[middle] {
+			if target > nums[middle] || target < nums[left] {
+				left = middle + 1
+			} else {
+				right = middle - 1
+			}
+		}
+		// if middle is less than right
+		else {
+			if target <= nums[middle] || target > nums[right] {
+				right = middle - 1
+			} else {
+				left = middle + 1
+			}
+		}
+	}
+	return -1
+}
+
 
 func main() {
 	n1 := []int{4,5,6,7,0,1,2}
 
-	fmt.Println(bruteForceSearch(n1, 0))
+	fmt.Println(binarySearch(n1, 0))
 
-	fmt.Println(bruteForceSearch(n1, 3))
+	fmt.Println(binarySearch(n1, 3))
 
 	n2 := []int{1}
 
-	fmt.Println(bruteForceSearch(n2, 0))
+	fmt.Println(binarySearch(n2, 0))
 }
