f9729064e670840e376222469178a6f36c35a9e1
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-07-09 21:31:16 -0700

    bleh

--- binary_search/rotated_sorted_array.go
+++ binary_search/rotated_sorted_array.go
@@ -24,16 +24,15 @@ func binarySearch(nums []int, target int) int {
 		if nums[middle] == target {
 			return middle
 		}
-		// if middle is less than left
+		// left sorted portion
 		if nums[left] <= nums[middle] {
 			if target > nums[middle] || target < nums[left] {
 				left = middle + 1
 			} else {
 				right = middle - 1
 			}
-		}
-		// if middle is less than right
-		else {
+		} else {
+			// right sorted portion
 			if target <= nums[middle] || target > nums[right] {
 				right = middle - 1
 			} else {
@@ -46,6 +45,7 @@ func binarySearch(nums []int, target int) int {
 
 
 func main() {
+
 	n1 := []int{4,5,6,7,0,1,2}
 
 	fmt.Println(binarySearch(n1, 0))
