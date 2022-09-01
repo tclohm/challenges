8ca6161c944647a160a7c238b2be6bc194a0f18b
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-09-01 13:04:00 -0700

    rock paper scissor yay

--- backtracking/rps.go
+++ backtracking/rps.go
@@ -6,25 +6,28 @@ func rps(players int) [][]string {
 	var moves = []string{"rock", "paper", "scissors"}
 	var games [][]string
 	var results []string
-	return dfs(players, &results, &games, moves)
+	// MARK : -- if we remove our index, which is 0
+	// 			 we get the permutations
+	// 			 index allows us to constrain the permutations
+	return dfs(players, &results, &games, moves, 0)
 }
 
-func dfs(players int, results *[]string, games *[][]string, moves []string) [][]string {
+func dfs(players int, results *[]string, games *[][]string, moves []string, index int) [][]string {
 	if players < 1 {
 		*games = append(*games, *results)
 		return *games
 	}
 
-	for _, move := range moves {
+	for i := index ; i < len(moves) ; i++ {
 		tmp := []string{}
 		tmp = append(tmp, *results...)
-		tmp = append(tmp, move)
-		dfs(players - 1, &tmp, games, moves)
+		tmp = append(tmp, moves[i])
+		dfs(players - 1, &tmp, games, moves, i)
 	}
 
 	return *games
 }
 
 func main() {
-	fmt.Println(rps(1))
+	fmt.Println(rps(3))
 }
