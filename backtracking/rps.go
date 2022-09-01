997e095f3ac998a7744fc897aeeac26d470ef8b9
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-09-01 14:47:03 -0700

    playing around with bt

--- backtracking/rps.go
+++ backtracking/rps.go
@@ -9,25 +9,27 @@ func rps(players int) [][]string {
 	// MARK : -- if we remove our index, which is 0
 	// 			 we get the permutations
 	// 			 index allows us to constrain the permutations
-	return dfs(players, &results, &games, moves, 0)
+	dfs(players, &results, &games, moves, 0)
+	return games
 }
 
-func dfs(players int, results *[]string, games *[][]string, moves []string, index int) [][]string {
+func dfs(players int, results *[]string, games *[][]string, moves []string, index int) {
 	if players < 1 {
 		*games = append(*games, *results)
-		return *games
+		return
 	}
 
 	for i := index ; i < len(moves) ; i++ {
+		// MARK: -- new array for each decision
 		tmp := []string{}
 		tmp = append(tmp, *results...)
 		tmp = append(tmp, moves[i])
 		dfs(players - 1, &tmp, games, moves, i)
 	}
 
-	return *games
+	return
 }
 
 func main() {
-	fmt.Println(rps(3))
+	fmt.Println(rps(2))
 }
