2e7c2c3bac2df3fa775e11e3bb60ef3ebd9f00c3
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-12-08 11:34:11 -0800

    pointer for typedout

--- redundancy/giq/typedOutStrings.go
+++ redundancy/giq/typedOutStrings.go
@@ -29,6 +29,44 @@ func typedOut(s, t string) bool {
 	return result == result2
 }
 
+func pointersTypedOut(s, t string) bool {
+	var p1, p2 = len(s)-1, len(t)-1
+
+	for p1 >= 0 || p2 >= 0 {
+		if s[p1] == '#' || t[p2] == '#' {
+			if s[p1] == '#' {
+				var backcount = 2
+				for backcount > 0 {
+					p1--
+					backcount--
+					if s[p1] == '#' {
+						backcount = 2
+					}
+				}
+			}
+
+			if t[p2] == '#' {
+				var backcount = 2
+				for backcount > 0 {
+					p2--
+					backcount--
+					if t[p2] == '#' {
+						backcount = 2
+					}
+				}
+			}
+		} else {
+			if s[p1] != t[p2] { 
+				return false 
+			} else {
+				p1--
+				p2--
+			}
+		}
+	}
+	return true
+}
+
 func main() {
 	a1 := "ab#z"
 	a2 := "az#z"
@@ -42,4 +80,8 @@ func main() {
 	fmt.Println(typedOut(a1, a2))
 	fmt.Println(typedOut(b1, b2))
 	fmt.Println(typedOut(c1, c2))
+
+	fmt.Println(pointersTypedOut(a1, a2))
+	fmt.Println(pointersTypedOut(b1, b2))
+	fmt.Println(pointersTypedOut(c1, c2))
 }
