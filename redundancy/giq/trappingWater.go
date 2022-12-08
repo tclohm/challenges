16bee518f3f860a8ffe55584c36edaf381e275e9
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-12-08 09:43:46 -0800

    trapped water

--- redundancy/giq/trappingWater.go
+++ redundancy/giq/trappingWater.go
@@ -42,8 +42,41 @@ func trapping(water []int) int {
 	
 }
 
+func opTrap(height []int) int {
+	// 1. Id pointer with lesser value
+	// 2. Is this pointer lesser than equal to the max of that side
+	// 		if true, update max
+	// 		if not, get water for pointer and add to total
+	// 3. Move pointer inward
+	// 4. repeat for other pointer
+	var maxLeft, maxRight = 0, 0
+	var left, right = 0, len(height) - 1
+	var total = 0
+
+	for left < right {
+		if height[left] <= height[right] {
+			if height[left] >= maxLeft {
+				maxLeft = height[left]
+			} else {
+				total += maxLeft - height[left] 
+			}
+			left += 1
+		} else {
+			if height[right] >= maxRight {
+				maxRight = height[right]
+			} else {
+				total += maxRight - height[right]
+			}
+			right -= 1
+		}
+	}
+	return total
+}
+
 func main() {
 	trapped := []int{0,1,0,2,1,3,1,0,1,2}
 
-	fmt.Println(trapping(trapped))
+	//fmt.Println(trapping(trapped))
+
+	fmt.Println(opTrap(trapped))
 }
