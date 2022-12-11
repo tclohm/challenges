698b55da3fcc585955d4f8de1de3e451adc8dbe0
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-12-11 15:10:00 -0800

    isalmostpalindrome

---
+++ redundancy/giq/palindrome.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func strip(s string) string {
+	stripped := ""
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			stripped += string(unicode.ToLower(r))
+		} 
+	}
+	return stripped
+}
+
+func isPalindrome(s string, left, right *int) bool {
+	for *left < *right {
+		if s[*left] != s[*right] {
+			return false
+		}
+		*left++
+		*right--
+	}
+	return true
+}
+
+func isAlmostPalindrome(s string) bool {
+	scrubbed := strip(s)
+	var left, right = 0, len(scrubbed) - 1
+	if (isPalindrome(scrubbed, &left, &right)) {
+		return true
+	}
+
+	for (left < right) {
+		if scrubbed[left] != scrubbed[right] {
+			leftPlus := left+1
+			rightMinus := right-1
+			return isPalindrome(scrubbed, &leftPlus, &right) || isPalindrome(scrubbed, &left, &rightMinus)
+		}
+	}
+	
+
+	return false
+}
+
+func main() {
+	s := "A man, a plan, a canal: Panama"
+	fmt.Println(isAlmostPalindrome(s))
+
+	s1 := "race a car"
+	fmt.Println(isAlmostPalindrome(s1))
+}
