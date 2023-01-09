003eb2104bc8c20a0a9914cc2c1f9d49a344fa22
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-01-09 14:14:27 -0800

    scheduler

--- redundancy/giq/graph/graphs2.go
+++ redundancy/giq/graph/graphs2.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+type Queue struct {
+	Container []int
+}
+
+func (q *Queue) Push(item int) {
+	q.Container = append(q.Container, item)
+}
+
+func (q *Queue) Pop() int {
+	var node = q.Container[0]
+	q.Container = q.Container[1:]
+	return node
+}
+
+func (q *Queue) Length() int {
+	return len(q.Container)
+}
+
 func max(a, b int) int {
 	if a > b { return a }
 	return b
@@ -17,6 +35,15 @@ func makeAdjacentList(n int, managers []int) [][]int {
 		var manager = managers[e]
 		if manager == -1 { continue }
 		al[manager] = append(al[manager], e)
+	}
+	return al
+}
+
+func makeAL(n int, courses adjList) [][]int {
+	var al = make(adjList, n)
+	for i := 0 ; i < len(courses) ; i++ {
+		current, next := courses[i][0], courses[i][1]
+		al[next] = append(al[next], current)
 	}
 	return al
 }
@@ -37,6 +64,39 @@ func numOfMinutes(n, headId int, managers, informTime []int) int {
 	al := makeAdjacentList(n, managers)
 	return dfs(headId, al, informTime)
 }
+
+// Course Scheduler
+func canFinish(n int, prerequisites adjList) bool {
+	var al = makeAL(n, prerequisites)
+	
+	for v := 0 ; v < n ; v++ {
+		q := Queue{}
+		seen := map[int]bool{}
+
+		for i := 0 ; i < len(al[v]) ; i++ {
+			q.Push(al[v][i])
+		}
+
+		for q.Length() > 0 {
+			var current = q.Pop()
+			seen[current] = true
+
+			if current == v { return false }
+
+			var adjacent = al[current]
+
+			for i := 0 ; i < len(adjacent) ; i++ {
+				var next = adjacent[i]
+				if _, ok := seen[next]; !ok {
+					q.Push(next)
+				}
+			}
+		}
+	}
+
+	return true
+}
+
 
 func main() {
 	// a company has n employees with unique IDs from 0 to n-1
@@ -62,6 +122,16 @@ func main() {
 	informTime := []int{0, 0, 4, 0, 7, 3, 6, 0}
 
 	// number of minutes it takes to inform all employees of the news
+	// 			4
+	// 	  	  / | \
+	// 	 	 5	2  6
+	// 		/  / \  \
+	//     7  0   1  3
 
 	fmt.Println(numOfMinutes(totalEmployee, headOfCompany, managers, informTime))
+
+	fmt.Println("-----")
+
+	prereqs := [][]int{{1, 0}, {2, 1}, {2, 5}, {0, 3}, {4, 3}, {3, 5}, {4, 5}}
+	fmt.Println(canFinish(6, prereqs))
 }
