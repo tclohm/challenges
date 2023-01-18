af0b42e356740076a22c417a509cb50ec538e14d
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-01-18 09:19:00 -0800

    day 6 done

---
+++ aoc/day6/part2.go
@@ -0,0 +1,34 @@
+package main
+
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main() {
+	file, _ := os.Open("file.txt")
+
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+
+	sc.Scan()
+
+	const differentCharacters = 14
+
+	for i := range sc.Text() {
+		set := make(map[byte]bool)
+		
+		for j := 0 ; j < differentCharacters ; j++ {
+			fmt.Println(sc.Text()[i+j])
+			set[sc.Text()[i+j]] = true
+		}
+
+		if len(set) == differentCharacters {
+			fmt.Println(i + differentCharacters)
+			break
+		}
+	}
+}
