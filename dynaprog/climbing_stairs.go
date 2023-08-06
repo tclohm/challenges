25823e8e6ca7cc32d63c3d1306037afb08f61501
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-08-06 12:56:58 -0700

    optimized

--- dynaprog/climbing_stairs.go
+++ dynaprog/climbing_stairs.go
@@ -5,6 +5,8 @@ import (
 )
 
 // climb stairs, each time you can take 1 or 2 steps. How many distinct ways can you climb to the top?
+// Time: O(n)
+// Space: O(n)
 func climb(stairs int) int {
 	var cache = make([]int, stairs+1)
 	return climbing(stairs, cache)
@@ -22,6 +24,42 @@ func climbing(stairs int, cache []int) int {
 	result := climbing(stairs - 1, cache) + climbing(stairs - 2, cache)
 	cache[stairs] = result
 	return result
+}
+
+// Bottom up
+// Time: O(n)
+// Space: O(n)
+
+func climbAgain(n int) int {
+	if n < 3 {
+		return n
+	}
+
+	ways := make([]int, n+1)
+	ways[1] = 1
+	ways[2] = 2
+
+	for i := 3 ; i <= n ; i++ {
+		ways[i] = ways[i-1] + ways[i-2]
+	}
+
+	return ways[n]
+}
+
+// Bottom up (Optimized)
+// Time: O(n)
+// Space: O(1)
+
+func climbOptimized(n int) int {
+	if n < 3 {
+		return n
+	}
+
+	prev, current := 1, 2
+	for i := 3 ; i <= n ; i++ {
+		prev, current = current, prev+current
+	}
+	return current
 }
 
 func main() {
