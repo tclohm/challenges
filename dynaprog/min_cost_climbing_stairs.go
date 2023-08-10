9ddf9e3dcce37cb0b00555e45f65827ad2a4493f
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-08-10 14:46:38 -0700

    starting min cost

---
+++ dynaprog/min_cost_climbing_stairs.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// [10, 15, 20]
+// 1, 2 steps
+
+// 						0 steps
+// 				10 					15
+// 			15			20		20 		end
+// 		20 		end end 	end end end
+
+func min_cost(stairs []int) int {
+	min_cost := 0
+	
+	return min_cost
+}
