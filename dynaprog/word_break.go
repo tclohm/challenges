787f6cb52bcae71ddb7c96575a3977b640d31368
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-08-13 16:16:54 -0700

    word brak

---
+++ dynaprog/word_break.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func wordBreak(s string, wordDict []string) bool {
+	dp := make([]bool, len(s) + 1)
+	dp[len(s)] = true
+
+	for i := len(s) - 1 ; i >= 0 ; i-- {
+		for _, w := range wordDict {
+			if (i + len(w)) <= len(s) && s[i: i + len(w)] == w {
+				dp[i] = dp[i + len(w)]
+			}
+			if dp[i] {
+				break
+			}
+		}
+	}
+	return dp[0]
+}
+
+
+func main() {
+	fmt.Println(wordBreak("leetcode",[]string{"leet","code"}))
+	fmt.Println(wordBreak("applepenapple",[]string{"apple","pen"}))
+	fmt.Println(wordBreak("catsandog",[]string{"cats","dog","sand","and","cat"}))
+}
