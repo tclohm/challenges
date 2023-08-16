ae15464462ceb6ce317e3ea79ec1f2e9ef77c769
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-08-16 16:48:34 -0700

    i suck at dp

---
+++ dynaprog/maximum_product_subarray.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// find a subarray that has the largest product
+func max_product(nums []int) int {
+	return -1
+}
+
+func main() {
+	fmt.Println(max_product([]int{2, 3, -2, 4}))
+}
