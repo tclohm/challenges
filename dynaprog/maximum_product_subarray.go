ddeb04947decd760ee3b85acbb31d45d3303de01
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-08-17 14:35:51 -0700

    ok

--- dynaprog/maximum_product_subarray.go
+++ dynaprog/maximum_product_subarray.go
@@ -1,12 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "math"
+)
 
 // find a subarray that has the largest product
+// declare two variables for tracking the min and max product value
+// track min for negative num because if we get two negs, they turn positive
 func max_product(nums []int) int {
-	return -1
+	min_value := nums[0]
+	max_value := nums[0]
+	max_product := nums[0]
+
+	for i := 1 ; i < len(nums) ; i++ {
+		if nums[i] < 0 {
+			min_value, max_value = max_value, min_value 
+		}
+
+		min_value = min(nums[i], min_value * nums[i])
+		max_value = max(nums[i], max_value * nums[i])
+
+		max_product = max(max_value, max_product)
+	}
+
+	return max_product
+}
+
+func min(a, b int) int {
+	if a < b { return a }
+	return b
+}
+
+func max(a, b int) int {
+	if a > b { return a }
+	return b
 }
 
 func main() {
-	fmt.Println(max_product([]int{2, 3, -2, 4}))
+	// subarray -> [2][2,3][2,3,-2][3,-2][3,-2,4][-2,4] 
+	fmt.Println(max_product([]int{2, 3, -2, 4})) // output 6 because [2,3]
 }
