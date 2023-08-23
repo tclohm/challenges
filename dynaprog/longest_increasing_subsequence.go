285ed43b52f7bf6e791373826f5c6fa263f41a5b
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-08-23 10:30:25 -0700

    tabulation

--- dynaprog/longest_increasing_subsequence.go
+++ dynaprog/longest_increasing_subsequence.go
@@ -12,8 +12,42 @@ import "fmt"
 // how do we compute the longest possible sequence at a given index? 
 // The way to do that is to look at maximum lengths of sequences at all previous 
 // indices and find the highest one that corresponds to a value that is lower then the current one
-func lengthOfLIS(nums []int) int {
 
+// ex: [10, 9, 2, 5, 3, 7, 101, 18]
+// lisLength[0] = 1
+// lisLength[1] = 1
+// lisLength[2] = 1
+// lisLength[3] = ?
+
+// iteratively, look at the previous values in lisLengths
+//	a. if the corresponding value is lower (index 2, input[2] < input[3]) then that's a candidate
+//     for finding the longest previous sequence
+//  b. check if we haven't found a higher one earlier and if we haven't, set the value
+
+// overlapping subproblems, dp problem
+func lengthOfLIS(nums []int) int {
+	lisLengths := make([]int, len(nums))
+
+	for i := range lisLengths {
+		lisLengths[i] = 1
+	}
+
+	for i := 1 ; i < len(nums) ; i++ {
+		for j := 0 ; j < i ; j++ {
+			if nums[j] < nums[i] && lisLengths[j] + 1 > lisLengths[i] {
+				lisLengths[i] = lisLengths[j] + 1
+			}
+		}
+	}
+
+	var max int
+	for _, length := range lisLengths {
+		if length > max {
+			max = length
+		}
+	}
+
+	return max
 }
 
 func main() {
