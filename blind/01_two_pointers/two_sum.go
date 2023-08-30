915c114bd5ae2e1be2e72d5ecbb016a0fc17eed1
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-08-30 11:23:21 -0700

    pointer two sum

---
+++ blind/01_two_pointers/two_sum.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func twoSum(numbers []int, target int) []int {
+	start := 0
+	end := len(numbers) - 1
+   
+   	for start != end {
+   		if numbers[end] + numbers[start] == target { return []int{start+1, end+1} }
+   		if numbers[end] + numbers[start] < target { start++ }
+   		if numbers[end] + numbers[start] > target { end-- }
+   	}
+    	
+    return []int{0}
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2,7,11,15}, 9))
+	fmt.Println(twoSum([]int{2,3,4}, 6))
+	fmt.Println(twoSum([]int{-1, 0}, -1))
+}
