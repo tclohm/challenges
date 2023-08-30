fc80e5ba65f506eb2885d2c1ec073e230a9c4641
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-08-30 10:57:48 -0700

    sodoku

--- blind/00_arrays_and_hashing/sodoku.go
+++ blind/00_arrays_and_hashing/sodoku.go
@@ -3,7 +3,28 @@ package main
 import "fmt"
 
 func isValidSudoku(board [][]string) bool {
-    return false
+	hm := make(map[string]bool)
+
+	for row := 0 ; row < 9 ; row++ {
+		for col := 0 ; col < 9 ; col++ {
+			value := string(board[row][col])
+
+			if value == "." { continue }
+
+			_, exist_row := hm[value + "in row" + string(row)]
+			_, exist_col := hm[value + "in col" + string(col)]
+			_, exist_gri := hm[value + "in grid" + string(row/3) + "-" + string(col/3)]
+
+			if exist_row || exist_col || exist_gri { 
+				return false 
+			} else {
+				hm[value + "in row" + string(row)] = true
+				hm[value + "in col" + string(col)] = true
+				hm[value + "in grid" + string(row/3) + "-" + string(col/3)] = true
+			}
+		}
+	}
+    return true
 }
 
 func main() {
