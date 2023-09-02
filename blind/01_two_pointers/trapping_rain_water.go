9a14461c99ea4acd4f061227a7378e0e8a202044
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-09-01 20:38:38 -0700

    starting trap

---
+++ blind/01_two_pointers/trapping_rain_water.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func trap(height []int) int {
+    return 0
+}
+
+func main() {
+	fmt.Println(trap([]int{0,1,0,2,1,0,1,3,2,1,2,1}))
+	fmt.Println(trap([]int{4,2,0,3,2,5}))
+}
