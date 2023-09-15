f2e2c8b6864a6b570e9cfff12955f8e89a7d05fd
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-09-14 20:40:10 -0700

    0s

--- blind/trees/kthnode.go
+++ blind/trees/kthnode.go
@@ -27,7 +27,7 @@ func kthSmallest(root *tree.Node, k int) int {
 }
 
 func main() {
-	one := tree.Build_binary_tree([]int{3,1,4,1,2})
+	one := tree.Build_binary_tree([]int{3,1,4,0,2})
 	six := tree.Build_binary_tree([]int{5,3,6,2,4,0,0,1})
 	fmt.Println(kthSmallest(one, 1))
 	fmt.Println(kthSmallest(six, 3))
