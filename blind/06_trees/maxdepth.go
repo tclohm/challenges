1ac7df69a724a79f05e11bf10a3b2c45f477d65d
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-09-19 08:20:11 -0700

    max depth

--- blind/06_trees/maxdepth.go
+++ blind/06_trees/maxdepth.go
@@ -6,25 +6,23 @@ import (
 )
 
 func maxdepth(root *tree.Node) int {
+	// right + 1, left + 1
 	if root == nil { return 0 }
 
-	left := maxdepth(root.Left)
+	left := maxdepth(root.Left) 
 	right := maxdepth(root.Right)
-
-	if left > right {
-		return 1 + left
-	}
-
+	
+	if left > right { return left + 1 }
 	return right + 1
 }
 
 func main() {
 	r := tree.Build_binary_tree([]int{1,2,3,4,5,6,7})
 	tree.Print_binary_tree(r, 0, "Root: ")
+	fmt.Println("max depth:", maxdepth(r))
 	tree.Line_break()
-	fmt.Println(maxdepth(r))
 	l := tree.Build_binary_tree([]int{1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17})
 	tree.Print_binary_tree(l, 0, "Root: ")
-	fmt.Println(maxdepth(l))
+	fmt.Println("max depth:", maxdepth(l))
 
 }
