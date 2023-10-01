5ed00616a23d032315eb582a5c93f86ca87d6603
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-10-01 14:31:27 -0700

    comments

--- blind/10_graph/schedule.go
+++ blind/10_graph/schedule.go
@@ -6,7 +6,7 @@ type doulbeArray [][]int
 
 func canFinish(numberOfCourses int, prerequisites doulbeArray) bool {
 	graph := make(map[int][]int)
-
+	// build an adjacency graph
 	for _, course := range prerequisites {
 
 		first_course, next_course := course[1], course[0]
@@ -19,17 +19,17 @@ func canFinish(numberOfCourses int, prerequisites doulbeArray) bool {
 
 	var dfs func(course int) bool
 	dfs = func(course int) bool {
-
+		// check if visited
 		if _, ok := visit[course]; ok {
 			return false
 		}
-
+		// if the course's prereq doesnt exist, we good!
 		if len(graph[course]) == 0 {
 			return true
 		}
-
+		// visit
 		visit[course] = struct{}{}
-
+		// move through our adjacency graph
 		for _, prerequisite := range graph[course] {
 			if !dfs(prerequisite) { return false }
 		}
