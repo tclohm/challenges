daa1d6e01ff62638e619651faea2634619ba135a
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-10-12 11:11:54 -0700

    palin substring

--- blind/12_dp/psubstring.go
+++ blind/12_dp/psubstring.go
@@ -3,7 +3,26 @@ package main
 import "fmt"
 
 func countSubstrings(s string) int {
-	return 0
+	length := len(s)
+	var palindrome func (int, int) int
+	palindrome = func (left, right int) int {
+		count := 0
+		for left >= 0 && right < length && s[left] == s[right] {
+			count++
+			left--
+			right++
+		}
+		return count
+	}
+
+	count := 0
+	for index, _ := range s {
+		// even
+		count += palindrome(index, index)
+		// odd
+		count += palindrome(index, index+1)
+	}
+	return count
 }
 
 func main() {
