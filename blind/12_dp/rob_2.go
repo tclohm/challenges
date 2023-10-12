6167346a1ca9e9771142c6aa596870d907c7911a
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-10-11 19:42:21 -0700

    hr2

--- blind/12_dp/rob_2.go
+++ blind/12_dp/rob_2.go
@@ -4,12 +4,34 @@ import "fmt"
 
 func rob(nums []int) int {
 	if len(nums) == 1 { return nums[0] }
-	return -1
+
+	if len(nums) == 2 {
+		return max(nums[0], nums[1])
+	}
+
+	return max(robbing(nums, 0, len(nums)-1), robbing(nums, 1, len(nums)))
+}
+
+func robbing(nums []int, start, end int) int {
+	dp := make([]int, len(nums))
+	dp[start] = nums[start]
+	dp[start + 1] = max(nums[start], nums[start + 1])
+
+	for i := start + 2 ; i < end ; i++ {
+		dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+	}
+
+	return dp[end - 1]
+}
+
+func max(a, b int) int {
+	if a > b { return a }
+	return b
 }
 
 func main() {
-	fmt.Println([]int{2,3,2})
-	fmt.Println([]int{1,2,3,1})
+	fmt.Println(rob([]int{2,3,2}))
+	fmt.Println(rob([]int{1,2,3,1}))
 }
 
 // choose 2 or 3
