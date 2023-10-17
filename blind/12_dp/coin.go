6b9c3799bf9898adbfd9a11b18ccb7d7d4ad1042
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-10-17 09:37:59 -0700

    dp bottom up

--- blind/12_dp/coin.go
+++ blind/12_dp/coin.go
@@ -1,21 +1,25 @@
 package main
 
 import "fmt"
-
+// bottom up -- what's the minimum amount of coins does it take to get to 0
+// now what's the minimum amount of coins to get to 1, and then 2, then 3, then 4
+// use the knowledge of previous calcuations
 func change(coins []int, amount int) int {
+	// dp
 	changes := make([]int, amount+1)
-
+	// make all the change in the array the amount + 1
 	for i := range changes {
 		changes[i] = amount + 1
 	}
-	
+	// first change is 0
 	changes[0] = 0
-
+	// now iterate through the amount with a nested for loop
 	for i := 1 ; i < amount + 1 ; i++ {
 		for _, c := range coins {
 			if c <= i {
 				changes[i] = min(changes[i], 1 + changes[i - c])
 			}
+			fmt.Println(changes)
 		}
 	}
 
