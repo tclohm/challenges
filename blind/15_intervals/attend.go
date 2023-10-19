c90ec567f1d7f30da9cdda15900c967c8c50ca54
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-10-19 15:45:25 -0700

    some more tests

--- blind/15_intervals/attend.go
+++ blind/15_intervals/attend.go
@@ -11,9 +11,14 @@ type Interval struct {
 
 func canAttend(intervals []*Interval) bool {
 	sort.Slice(intervals, func(a, b int) bool { return intervals[a].Start < intervals[b].Start })
-	headEnd  := intervals[0].End
+	headStart, headEnd  := intervals[0].Start, intervals[0].End
 	for i := 1 ; i < len(intervals) ; i++  {
+
 		if intervals[i].End < headEnd { return false }
+		//0 < 5 < 30 < 40
+		if headStart < intervals[i].Start && intervals[i].Start < headEnd { return false }
+		
+		headStart = intervals[i].Start
 		headEnd = intervals[i].End
 	}
 
@@ -21,6 +26,6 @@ func canAttend(intervals []*Interval) bool {
 }
 
 func main() {
-	fmt.Println(canAttend([]*Interval{&Interval{0,30},&Interval{5,10},&Interval{15,20}}))
+	fmt.Println(canAttend([]*Interval{&Interval{0,30},&Interval{5,20},&Interval{15,25}}))
 	fmt.Println(canAttend([]*Interval{&Interval{5,8},&Interval{9,15}}))
 }
