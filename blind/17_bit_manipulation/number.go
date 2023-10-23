803702f15284cddd1037d45f1e77d42bf6a8f37c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-10-23 14:27:19 -0700

    bit operation

--- blind/17_bit_manipulation/number.go
+++ blind/17_bit_manipulation/number.go
@@ -1,3 +1,18 @@
 package main
 
 import "fmt"
+
+func hammingWeight(num uint32) int {
+	res := 0
+	for num > 0 {
+		num &= num - 1
+		res += 1
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(hammingWeight(00000000000000000000000000001011))
+	fmt.Println(hammingWeight(00000000000000000000000010000000))
+	fmt.Println(hammingWeight(00000000000000000000011111111111))
+}
