e004a722f1253aecc5755b7b66d7906641cb5a57
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-11-02 17:24:23 -0700

    holy fuck

--- blind/10_graph/rotting.go
+++ blind/10_graph/rotting.go
@@ -2,11 +2,31 @@ package main
 
 import "fmt"
 
+type queue [][]int
+
+func constructor() *queue {
+	return &queue{}
+}
+
+func (q *queue) push(i []int) {
+	*q = append(*q, i)
+}
+
+func (q *queue) pop() []int {
+	el := (*q)[0]
+	(*q) = (*q)[1:]
+	return el
+}
+
+func (q *queue) length() int {
+	return len((*q))
+}
+
 // 0 empty
 // 1 fresh
 // 2 rotten
 func rotting(grid [][]int) int {
-	q := [][]int{}
+	q := constructor()
 	fresh := 0
 	time := 0
 	for r := 0 ; r < len(grid) ; r++ {
@@ -15,25 +35,24 @@ func rotting(grid [][]int) int {
 				fresh += 1
 			}
 			if grid[r][c] == 2 {
-				q = append(q, []int{r, c})
+				q.push([]int{r,c})
 			}
 		}
 	}
 
 	directions := [4][2]int{{0,1},{0,-1},{1,0},{-1,0}}
 
-	for fresh > 0 && len(q) != 0 {
-		for i := 0 ; i < len(q) ; i++ {
+	for fresh > 0 && q.length() != 0 {
+		for i := 0 ; i < q.length() ; i++ {
 
-			item := q[0]
-			q = q[1:]
+			item := q.pop()
 
 			for _, dir := range directions {
 				row, col := item[0] + dir[0], item[1] + dir[1]
 
 				if row >= 0 && row < len(grid) && col >= 0 && col < len(grid[0]) && grid[row][col] == 1 {
 					grid[row][col] = 2
-					q = append(q, []int{row, col})
+					q.push([]int{row,col})
 					fresh -= 1
 				}
 			}
