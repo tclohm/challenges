9fd9dc2bdde3bbc2da31c7dd1ead5f58588e303c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-12-11 15:05:35 -0800

    working hashmap

--- blind/00_arrays_and_hashing/build_hash.go
+++ blind/00_arrays_and_hashing/build_hash.go
@@ -9,56 +9,92 @@ type Node struct {
 }
 
 type Hashmap struct {
-	container [1000]Node
+	array []*Node
+	size int
 }
 
-func Init() *Hashmap {
-	h := &Hashmap{}
-	for i := 0 ; i < 1000 ; i++ {
-		h.container[i] = Node{}
+func Init() Hashmap {
+	return Hashmap{ 
+		size: 1000, 
+		array: make([]*Node, 1000),
 	}
-	return h
 }
 
 func (self *Hashmap) hash(key int) int {
-	return key % len(self.container)
+	//fmt.Println("hash", key % len(self.array))
+	return key % len(self.array)
 }
 
 func (self *Hashmap) put(key int, value int) {
-	var current Node = self.container[self.hash(key)]
-	for current.next != nil {
+	var position = self.hash(key)
+	var newNode *Node = &Node{
+		key: key,
+		val: value,
+		next: nil,
+	}
+
+	if self.array[position] == nil {
+		self.array[position] = newNode
+		return
+	}
+
+	var current *Node = self.array[position]
+
+	for current != nil {
 		if current.key == key {
 			current.val = value
 			return
 		}
-		current = *current.next
+		current = current.next
 	}
-	current.next = &Node{key, value, nil}
+
+	newNode.next = self.array[position]
+	self.array[position] = newNode
 }
 
 func (self *Hashmap) get(key int) int {
-	var current Node = self.container[self.hash(key)]
-	for current.next != nil {
+	var position = self.hash(key)
+
+	var current *Node = self.array[position]
+
+	for current != nil {
 		if current.key == key {
 			return current.val
 		}
-		current = *current.next
+		current = current.next
 	}
 	return -1
 }
 
 func (self *Hashmap) remove(key int) {
-	var current Node = self.container[self.hash(key)]
-	for current.next != nil {
-		if current.next.key == key {
-			current.next = current.next.next
+	var position = self.hash(key)
+	var current *Node = self.array[position]
+	
+	if current == nil { return }
+
+	if current.key == key {
+		current = current.next
+		self.array[position] = current
+		return
+	}
+
+	var prev *Node
+
+	for current != nil {
+		if current.key == key {
+			prev.next = current.next
 			return
 		}
-		current = *current.next
+		prev = current
+		current = current.next
 	}
 }
 
 func main() {
 	m := Init()
-	fmt.Println(m)
+	m.put(100, 45)
+	m.put(5, 20)
+	m.put(999, 1)
+	fmt.Println(m.get(4))
+	fmt.Println(m.get(5))
 }
