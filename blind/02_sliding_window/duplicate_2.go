85268df340b00c043cb605a50593a793e7743623
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-12-12 07:10:41 -0800

    comment

--- blind/02_sliding_window/duplicate_2.go
+++ blind/02_sliding_window/duplicate_2.go
@@ -7,14 +7,16 @@ func duplicate(nums []int, k int) bool {
 	left := 0
 
 	for right, _ := range nums {
+		// bigger than k, remove left
 		if right - left > k {
 			delete(window, nums[left])
 			left += 1
 		}
+		// right in window
 		if _, ok := window[nums[right]]; ok {
 			return true
 		}
-
+		// move the right into window
 		window[nums[right]] = true
 	}
 
