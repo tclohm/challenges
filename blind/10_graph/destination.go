5c2ee6222d0c1abfa21fcef95b249ec6500f1beb
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-12-15 11:58:08 -0800

    comments

--- blind/10_graph/destination.go
+++ blind/10_graph/destination.go
@@ -4,17 +4,20 @@ import "fmt"
 
 func findDestination(paths [][]string) string {
 	ht := map[string]string{}
-
+	// put all the paths into the map
 	for _, points := range paths {
 		from, to := points[0], points[1]
 		ht[from] = to
 	}
-
+	// loop through the map 
+	// and check if the value, is a key in the map
+	// if not that's the end destination
 	for _, value := range ht {
 		if _, ok := ht[value]; !ok {
 			return value
 		}
 	}
+
 	return "-1"
 }
 
