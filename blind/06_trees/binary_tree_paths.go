686195cf772d5e16f6311ec62b96dcb2b5c1e0ca
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-01-16 08:33:38 -0800

    bt path

--- blind/06_trees/binary_tree_paths.go
+++ blind/06_trees/binary_tree_paths.go
@@ -8,10 +8,28 @@ import (
 func binaryTreePaths(root *tree.Node) []string {
 	result := []string{}
 
+	var dfs func (node *tree.Node, res []string)
+	dfs = func(node *tree.Node, res []string) {
+		res = append(res, fmt.Sprintf("%d", node.Value))
+		if node.Left == nil && node.Right == nil {
+			result = append(result, res...)
+		}
+
+		res = append(res, "->")
+		if node.Left != nil {
+			dfs(node.Left, res)
+		}
+		if node.Right != nil {
+			dfs(node.Right, res)
+		}
+	}
+
+	dfs(root, nil)
 	return result
 }
 
 func main() {
 	root := tree.Build_binary_tree([]int{1,2,3,0,5})
 	tree.Line_break()
+	fmt.Println(binaryTreePaths(root))
 }
