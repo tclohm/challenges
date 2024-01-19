00cf1ebc4f401adc5fd86ea6794e1b187968534c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-01-19 12:45:04 -0800

    so much repitition

---
+++ blind/09_backtracking/comb.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func comb(nums []int, target int) [][]int {
+	var list [][]int
+
+	var backtrack func (tmp []int, nums []int, remain int, start int)
+	backtrack = func(tmp []int, nums []int, remain, start int) {
+		if remain < 0 {
+			return 
+		} else if remain == 0 {
+			list = append(list, append([]int{}, tmp...))
+		} else {
+			for i := start ; i < len(nums) ; i++ {
+				tmp = append(tmp, nums[i])
+				backtrack(tmp, nums, remain - nums[i], i)
+				tmp = tmp[:len(tmp) - 1]
+			}
+		}
+	}
+
+	backtrack([]int{}, nums, target, 0)
+	return list
+}
+
+func main() {
+	fmt.Println(comb([]int{2,3,6,7}, 7))
+	fmt.Println(comb([]int{2,3,5}, 8))
+	fmt.Println(comb([]int{2}, 1))
+}
