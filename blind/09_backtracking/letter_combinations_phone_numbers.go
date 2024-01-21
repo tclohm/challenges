a9b765669509d049fad176283b45a76f0fd094e4
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-01-21 07:55:48 -0800

    backtracking can be beautiful

--- blind/09_backtracking/letter_combinations_phone_numbers.go
+++ blind/09_backtracking/letter_combinations_phone_numbers.go
@@ -3,25 +3,35 @@ package main
 import "fmt"
 
 func letterCombinations(digits string) []string {
-	if digits == "" { return []string{} }
-	var list []string
-	var phone = map[byte]string{'2':"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
-
-	var backtrack func(combination, digit string)
-	backtrack = func(combination, digit string) {
-		if digit == "" {
-			list = append(list, combination)
-		} else {
-			button := digit[0]
-			letters := phone[button]
-			for _, letter := range letters {
-				// next digit for combo
-				backtrack(combination + string(letter), digit[1:])
-			}
-		}
+	if len(digits) == 0 {
+		return []string{}
 	}
+
+	var result []string
+	var phonePad = map[byte]string{
+		'2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz",
+	}
+
+
+	var backtrack func(combination, digits string)
+	backtrack = func(combination string, digits string) {
+		
+		if digits == "" {
+			result = append(result, combination) 
+			return
+		}
+
+		var button = digits[0]
+		var pressed = phonePad[button]
+
+		for _, letter := range pressed {
+			backtrack(combination + string(letter), digits[1:])
+		}
+
+	}
+
 	backtrack("", digits)
-	return list	
+	return result
 }
 
 func main() {
