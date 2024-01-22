ffa65ff75163f67cba9786a18b2ae69de9d29b30
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-01-22 07:35:47 -0800

    splitting backtrack again

--- blind/09_backtracking/splitting.go
+++ blind/09_backtracking/splitting.go
@@ -6,28 +6,26 @@ import (
 )
 
 func splitString(s string) bool {
+	if s == "" { return false }
 
-	var backtrack func(index int, prev int) bool
-	backtrack = func(index int, prev int) bool {
+	var backtrack func (index int, previousValue int) bool
+	backtrack = func (index int, previousValue int) bool {
 		if index == len(s) { return true }
-		// we have the previous value in our parameters, move through the others and compare
 		for i := index ; i < len(s) ; i++ {
-			// value : start at index and add i + 1
-			v, _ := strconv.Atoi(string(s[index: i + 1]))
-			// if our prev - 1 equals value and our backtracking is true, return true
-			if prev - 1 == v && backtrack(i + 1, v) {
+			currentValue, err := strconv.Atoi(string(s[index:i + 1]))
+			if err != nil { return false }
+			if previousValue - 1 == currentValue && backtrack(i + 1, currentValue) {
 				return true
 			}
 		}
 		return false
 	}
 
-	// starting the run
-	// s - 1 because we need to have two things to compare
-	// if we go to the end, we will have nothing to compare the end to
+	// comparing two values
 	for i := 0 ; i < len(s) - 1 ; i++ {
-		v, _ := strconv.Atoi(string(s[:i + 1]))
-		if backtrack(i + 1, v) {
+		val, err := strconv.Atoi(string(s[:i+1]))
+		if err != nil { return false }
+		if backtrack(i + 1, val) {
 			return true
 		}
 	}
