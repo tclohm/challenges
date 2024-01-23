713aeca04f67d5e2095730bcfc5d899ad678a40c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-01-23 13:08:55 -0800

    not sure

--- blind/09_backtracking/unique_binary_string.go
+++ blind/09_backtracking/unique_binary_string.go
@@ -2,32 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func findDifferentBinaryString(nums []string) string {
-	//var set = make(map[byte]bool)
-	var result string
-	var backtrack func (index int, current []string) string
-	backtrack = func (index int, current []string) string {
-		if index == len(nums) {
-			result = strings.Join(current, ",")
-			return result
+	var result string = ""
+	for i, _ := range nums {
+		if string(nums[i][i]) == "0" {
+			result += "1" 
+		} else {
+			result += "0"
 		}
-
-		result = backtrack(index + 1, current)
-		if result != "" {
-			return result
-		}
-
-		current[index] = "1"
-		result = backtrack(index + 1, current)
-		if result != "" {
-			return result
-		}
-		return ""
 	}
-	return ""
+	return result
 }
 
 func main() {
