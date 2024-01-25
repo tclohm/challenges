7287b4da998436466d1a6ff79a27c83bfa4511e7
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-01-25 09:19:16 -0800

    backtracking solution

--- blind/09_backtracking/partition_k_equal_sum_subset.go
+++ blind/09_backtracking/partition_k_equal_sum_subset.go
@@ -51,7 +51,42 @@ func partition(nums []int, k int) bool {
 	return result == k
 }
 
+func part(nums []int, k int) bool {
+	var target = sum(nums) / k
+	var used = make([]bool, len(nums), len(nums))
+
+	var backtrack func(index int, k int, subsetSum int) bool
+	backtrack = func(index int, k int, subsetSum int) bool {
+		
+		if k == 0 {
+			return true
+		}
+
+		if subsetSum == target {
+			return backtrack(0, k - 1, 0)
+		}
+
+		for j := index ; j < len(nums) ; j++ {
+			if used[j] || subsetSum + nums[j] > target {
+				continue
+			} else {
+				used[j] = true
+				if backtrack(j + 1, k, subsetSum + nums[j]) {
+					return true
+				}
+				used[j] = false
+			}
+		}
+		return false
+	}
+
+	return backtrack(0, k, 0)
+}
+
 func main() {
 	fmt.Println(partition([]int{4,3,2,3,5,2,1}, 4))
 	fmt.Println(partition([]int{1,2,3,4}, 3))
+
+	fmt.Println(part([]int{4,3,2,3,5,2,1}, 4))
+	fmt.Println(part([]int{1,2,3,4}, 3))
 }
