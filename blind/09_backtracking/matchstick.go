b341a319716e67ade530fc5a8110f736ddbfe688
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-01-30 09:02:53 -0800

    match stick to square

---
+++ blind/09_backtracking/matchstick.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func makesquare(matchsticks []int) bool {
+	return true
+}
+
+func main() {
+	fmt.Println(makesquare([]int{1,1,2,2,2}))
+	fmt.Println(makesquare([]int{3,3,3,3,4}))
+}
