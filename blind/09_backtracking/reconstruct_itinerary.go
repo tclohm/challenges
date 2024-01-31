92be56448bf274f1bec3e9ab50c0a645ead8024f
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-01-31 08:46:03 -0800

    sort adj list values

--- blind/09_backtracking/reconstruct_itinerary.go
+++ blind/09_backtracking/reconstruct_itinerary.go
@@ -1,9 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func findItinerary(tickets [][]string) []string {
-	return []string{}
+
+	adjacent_list := map[string][]string{}
+
+
+	for _, ticket := range tickets {
+		src, dst := ticket[0], ticket[1]
+		adjacent_list[src] = append(adjacent_list[src], dst)
+	}
+
+	for _, ticket := range tickets {
+		src := ticket[0]
+		sort.Strings(adjacent_list[src])
+	}
+
+	var result = []string{"JFK"}
+
+	var backtrack func(src string) bool
+	backtrack = func(src string) bool {
+		if len(result) == len(tickets) + 1 {
+			return true
+		}
+		if len(adjacent_list[src]) == 0 {
+			return false
+		}
+
+		temporary := append([]string{}, adjacent_list[src]...)
+
+		for i, v := range temporary {
+			popped := adjacent_list[src][i]
+			adjacent_list[src] = append(adjacent_list[src][:i], adjacent_list[src][i+1:]...)
+			
+			result = append(result, v)
+
+			if backtrack(v) { 
+				return true 
+			}
+			if len(adjacent_list[src]) == i {
+				adjacent_list[src] = append(adjacent_list[src], popped)
+			} else {
+				adjacent_list[src] = append(adjacent_list[src][:i+1], adjacent_list[src][i:]...)
+				adjacent_list[src][i] = popped
+			}
+			
+		}
+		return false
+	}
+
+	backtrack("JFK")
+
+	return result
 }
 
 func main() {
