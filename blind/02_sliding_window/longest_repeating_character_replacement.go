7d33a8eb30c497b5885d9e42ba820256f1799bd2
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-02 10:45:26 -0800

    cmmt

--- blind/02_sliding_window/longest_repeating_character_replacement.go
+++ blind/02_sliding_window/longest_repeating_character_replacement.go
@@ -14,20 +14,21 @@ func characterReplacement(s string, k int) int {
 	left := 0
 
 	for right, _ := range s {
+		// add right pointer chars to the count
 		b := s[right]
 		count[b]++
-
+		// grab our window
 		window := (right - left + 1)
-
+		// is our window - right pointer count larger than k
 		if window - count[b] > k {
-			// remove left pointer
+			// if it is, remove letters from count using the left pointer and increment
 			lb := s[left]
 			count[lb]--
 			left++
 		}
-
+		// update our window
 		window = (right - left + 1)
-
+		// check if the longest is less than our window
 		if longest < window {
 			longest = window
 		}
