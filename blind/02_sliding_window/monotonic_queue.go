ea5c34f6f3f771f866e23e54c9d58169e1e8bcb9
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-04 17:54:05 -0800

    mono

--- blind/02_sliding_window/monotonic_queue.go
+++ blind/02_sliding_window/monotonic_queue.go
@@ -23,7 +23,32 @@ func slidingWindowMax(nums []int, k int) []int {
 	return result
 }
 
+func maxSlidingWindow(nums []int, k int) [] int {
+	deque := make([]int, 0, k)
+	result := make([]int, 0, len(nums) - k + 1)
+
+	for i, n := range nums {
+		// push
+		// remove all indices from deque that corresponds
+		// to numbers less than number we are trying to push
+		for len(deque) > 0 && nums[deque[len(deque) - 1]] < n {
+			deque = deque[:len(deque) - 1]
+		}
+		deque = append(deque, i)
+
+		if i < k - 1 { continue }
+
+		result = append(result, nums[deque[0]])
+
+		if deque[0] == i - k + 1 {
+			deque = deque[1:]
+		}
+	}
+
+	return result
+}
+
 func main() {
-	fmt.Println(slidingWindowMax([]int{1,3,-1,-3,5,3,6,7}, 3))
-	fmt.Println(slidingWindowMax([]int{1}, 1))
+	fmt.Println(maxSlidingWindow([]int{1,3,-1,-3,5,3,6,7}, 3))
+	fmt.Println(maxSlidingWindow([]int{1}, 1))
 }
