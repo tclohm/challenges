838bfcba5d959483d1c005956aea95eed1cb7499
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-06 17:17:05 -0800

    sliding window

--- blind/02_sliding_window/permutation_in_string.go
+++ blind/02_sliding_window/permutation_in_string.go
@@ -2,49 +2,22 @@ package main
 
 import "fmt"
 
+
+// compare finger prints
 func checkInclusion(s1, s2 string) bool {
-	if len(s1) > len(s2) { return false }
+	left, count := 0, [26]int{}
+	for i := range s1 { count[s1[i]-'a']++ }
 
-	s1Count, s2Count := make([]rune, 26, 26), make([]rune, 26, 26)
+	for right := range s2 {
+		count[s2[right]-'a']--
+		if count == [26]int{} { return true }
 
-	for i, _ := range s1 {
-		s1Count[s1[i] - 'a']++
-		s2Count[s2[i] - 'a']++
-	}
-
-	matches := 0
-	for i := 0 ; i < 26 ; i++ {
-		if s1Count[i] == s2Count[i] {
-			matches++
+		if right + 1 >= len(s1) {
+			count[s2[left]-'a']++
+			left++
 		}
 	}
-
-	left := 0
-	for right := len(s1) ; right < len(s2) ; right++ {
-		if matches == 26 { return true }
-
-		index := rune(s2[right]) - 'a'
-		s2Count[index]++
-
-		if s1Count[index] == s2Count[index] {
-			matches += 1
-		} else if s1Count[index] + 1 == s2Count[index] {
-			matches -= 1
-		}
-
-		index = rune(s2[left]) - 'a'
-		s2Count[index]--
-
-		if s1Count[index] == s2Count[index] {
-			matches += 1
-		} else if s1Count[index] - 1 == s2Count[index] {
-			matches -= 1
-		}
-
-		left++
-	}
-
-	return matches == 26
+	return false
 }
 
 func main() {
