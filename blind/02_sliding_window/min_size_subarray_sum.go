a170153af8688f1a0642c5791109f893e5676939
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-07 20:06:53 -0800

    whatever

--- blind/02_sliding_window/min_size_subarray_sum.go
+++ blind/02_sliding_window/min_size_subarray_sum.go
@@ -4,6 +4,31 @@ import (
 	"fmt"
 )
 
+func minSubArray(nums []int, target int) int {
+	l := 0
+	total := 0
+	count := 1000
+
+	for r, _ := range nums {
+		total += nums[r]
+		for total >= target {
+			count = min(r - l + 1, count)
+			total -= nums[l]
+			l += 1
+		}
+	}
+	if count == 1000 {
+		return 0
+	}
+	return count
+}
+
+func min(a, b int) int {
+	if a < b { return a }
+	return b
+}
+
 func main() {
-	
+	fmt.Println(minSubArray([]int{2,3,1,2,4,3}, 7))
+	fmt.Println(minSubArray([]int{1,4,4}, 4))
 }
