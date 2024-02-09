34701a5df3d7dd8c4123a11494aeadb11cd8b655
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-09 14:30:19 -0800

    memoized

--- blind/12_dp/stairs.go
+++ blind/12_dp/stairs.go
@@ -3,18 +3,29 @@ package main
 import "fmt"
 
 func climb(steps int) int {
-	if steps == 1 {
-		return 1
+	memo := map[int]int{}
+
+	var decision func(steps int) int
+	decision = func(steps int) int {
+		if steps == 1 {
+			return 1
+		}
+		if steps == 2 {
+			return 2
+		}
+
+		memo[steps - 1] = decision(steps - 1)
+		memo[steps - 2] = decision(steps - 2)
+
+		return memo[steps - 1] + memo[steps - 2]
 	}
-	if steps == 2 {
-		return 2
-	}
-	
-	return climb(steps - 1) + climb(steps - 2)
+
+	return decision(steps)
 }
 
 func main() {
 	fmt.Println(climb(2))
 	fmt.Println(climb(3))
+	fmt.Println(climb(4))
 	fmt.Println(climb(5))
 }
