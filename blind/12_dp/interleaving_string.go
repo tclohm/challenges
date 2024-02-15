9dac3cc86c33361bdaa1f3b70fe23d2e1782c16f
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-15 09:33:47 -0800

    err

--- blind/12_dp/interleaving_string.go
+++ blind/12_dp/interleaving_string.go
@@ -1,29 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func isInterleave(s1 string, s2 string, s3 string) bool {
 	if len(s1) + len(s2) != len(s3) { return false }
 
-	var isInterleaved func(p1, p2, p3 int) bool
-	isInterleaved = func(p1, p2, p3 int) bool {
-		if p3 == len(s3) {
-			if p1 == len(s1) && p2 == len(s2) { return true }
-			return false
+	// cache := make([][]bool, len(s1) + 1, len(s1) + 1)
+	// for i := range s2 {
+	// 	cache[i] = make([]bool, len(s2) + 1, len(s2) + 1)
+	// }
+	// cache[len(s2)] = make([]bool, len(s2) + 1, len(s2) + 1)
+
+	// cache[len(s1)][len(s1)] = true
+
+	var isInterleaved func(i, j int) bool
+
+	isInterleaved = func(i, j int) bool {
+		if i == len(s1) && j == len(s2) { return true }
+	
+		choose_s1, choose_s2 := false, false
+
+		if i < len(s1) && s1[i] == s3[i + j] {
+			choose_s1 = isInterleaved(i + 1, j)
+		}
+		if j < len(s2) && s2[j] == s3[i + j] {
+			choose_s2 = isInterleaved(i, j + 1)
 		}
 
-		result := false
-
-		if p1 < len(s1) && s1[p1] == s3[p3] {
-			result = result || isInterleaved(p1 + 1, p2, p3 + 1)
-		}
-		if p2 < len(s2) && s2[p2] == s3[p3] {
-			result = result || isInterleaved(p1, p2 + 1, p3 + 1)
-		}
-
-		return result
+		return choose_s1 || choose_s2
 	}
-	return isInterleaved(0, 0, 0)
+	return isInterleaved(0, 0)
 }
 
 func main() {
