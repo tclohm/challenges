452a184e112939ca19d0524720c85e7d71018437
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-19 13:58:44 -0800

    dfs, take or dont take

--- blind/12_dp/longest_increasing_subsequence.go
+++ blind/12_dp/longest_increasing_subsequence.go
@@ -34,8 +34,23 @@ func max(items []int) int {
 	return largest
 }
 
+func longest_increasing_sub(sequence []int) int {
+	var solve func(index int, prev int) int
+	solve = func(index int, prev int) int {
+		if index >= len(sequence) { return 0 }
+		take := 0
+		dont := solve(index + 1, prev)
+		if sequence[index] > prev {
+			take = 1 + solve(index + 1, sequence[index])
+		}
+		return bigger(take, dont)
+	}
+	return solve(0, -10)
+}
+
+// are we going to include it or not include it : 2 choices
 func main() {
-	fmt.Println(lengthOfLIS([]int{10,9,2,5,3,7,101,18}))
-	fmt.Println(lengthOfLIS([]int{0,1,0,3,2,3}))
-	fmt.Println(lengthOfLIS([]int{7,7,7,7,7,7,7}))
+	fmt.Println(longest_increasing_sub([]int{10,9,2,5,3,7,101,18}))
+	fmt.Println(longest_increasing_sub([]int{0,1,0,3,2,3}))
+	fmt.Println(longest_increasing_sub([]int{7,7,7,7,7,7,7}))
 }
