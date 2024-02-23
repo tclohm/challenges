e7603d8652f35e8205aa26064c327d5789aa507a
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-23 15:53:04 -0800

    dfs triangle

--- blind/12_dp/triangle.go
+++ blind/12_dp/triangle.go
@@ -9,13 +9,8 @@ func minimumTotal(triangle [][]int) int {
 		if y >= len(triangle) { return 0 }
 		if x > len(triangle[y]) { return 0 }
 
-		min := min(triangle[y][x], triangle[y][x + 1])
+		return min(triangle[y][x] + dfs(x, y + 1), triangle[y][x + 1] + dfs(x + 1, y + 1))
 
-		if min == triangle[y][x] {
-			return min + dfs(x, y+1)
-		}
-
-		return min + dfs(x+1, y+1)
 	}
 	return triangle[0][0] + dfs(0,1)
 }
@@ -23,4 +18,5 @@ func minimumTotal(triangle [][]int) int {
 func main() {
 	fmt.Println(minimumTotal([][]int{{2},{3,4},{6,5,7},{4,1,8,3}}))
 	fmt.Println(minimumTotal([][]int{{-10}}))
+	fmt.Println(minimumTotal([][]int{{2},{3,4},{6,5,7},{4,1,8,3},{3,1,4,2,0},{1,1,2,1,1,0}}))
 }
