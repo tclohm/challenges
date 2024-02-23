92af6de7ab4cae23122d62a739c332b840f84e5a
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-23 09:27:06 -0800

    top down cache

--- blind/13_2d_dp/maximal.go
+++ blind/13_2d_dp/maximal.go
@@ -3,7 +3,48 @@ package main
 import "fmt"
 
 func maximalSquare(matrix [][]string) int {
-    return 1
+	ROWS, COLS := len(matrix), len(matrix[0])
+	cache := map[string]int{}
+ 
+	var fillCache func (row, col int) int
+	fillCache = func (row, col int) int {
+		if row >= ROWS || col >= COLS { return 0 }
+
+		s := fmt.Sprintf("%d%d", row, col)
+		if _, ok := cache[s]; !ok {
+			down := fillCache(row + 1, col)
+			right := fillCache(row, col + 1)
+			diagonal := fillCache(row + 1, col + 1)
+
+			cache[s] = 0
+			if matrix[row][col] == "1" {
+				cache[s] = 1 + min(down, right, diagonal)
+			}
+		}
+		return cache[s]
+	}
+
+	fillCache(0, 0)
+
+	max := 0
+
+	for _, v := range cache {
+		if v > max {
+			max = v
+		}
+	}
+
+    return max * max
+}
+
+func min(a, b, c int) int {
+	if a < b && a < c {
+		return a
+	}
+	if b < a && b < c {
+		return b
+	}
+	return c
 }
 
 func main() {
