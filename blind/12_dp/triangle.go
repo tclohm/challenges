a38652c4270c861531c349f1f0cfc22bcec752c8
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-23 16:43:26 -0800

    dp

--- blind/12_dp/triangle.go
+++ blind/12_dp/triangle.go
@@ -15,8 +15,20 @@ func minimumTotal(triangle [][]int) int {
 	return triangle[0][0] + dfs(0,1)
 }
 
+func minimumDP(triangle [][]int) int {
+	dp := make([]int, len(triangle) + 1)
+
+	for i := len(triangle) - 1 ; i >= 0 ; i-- {
+		for i, n := range triangle[i] {
+			dp[i] = n + min(dp[i], dp[i+1])
+		}
+	}
+
+	return dp[0]
+}
+
 func main() {
-	fmt.Println(minimumTotal([][]int{{2},{3,4},{6,5,7},{4,1,8,3}}))
-	fmt.Println(minimumTotal([][]int{{-10}}))
-	fmt.Println(minimumTotal([][]int{{2},{3,4},{6,5,7},{4,1,8,3},{3,1,4,2,0},{1,1,2,1,1,0}}))
+	fmt.Println(minimumDP([][]int{{2},{3,4},{6,5,7},{4,1,8,3}}))
+	fmt.Println(minimumDP([][]int{{-10}}))
+	fmt.Println(minimumDP([][]int{{2},{3,4},{6,5,7},{4,1,8,3},{3,1,4,2,0},{1,1,2,1,1,0}}))
 }
