c507d730dea4a616d906d918675b3f718b469064
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-24 16:44:02 -0800

    unique bst

--- blind/12_dp/unique_bst.go
+++ blind/12_dp/unique_bst.go
@@ -5,19 +5,21 @@ import "fmt"
 // 2 => 2
 // 3 => 5
 // 4 => 
+// T: O(n**2) S O(n)
 func numTrees(n int) int {
-	G := make([]int, n + 1)
-	G[0] = 1
-	G[1] = 1
+	result := make([]int, n + 1)
+	result[0] = 1
+	result[1] = 1
 
 	for i := 2 ; i <= n ; i++ {
 		for j := 1 ; j <= i ; j++ {
-			G[i] += G[j - 1] * G[i - j]
+			result[i] += result[j - 1] * result[i - j]
 		}
 	}
-	return G[n]
+	return result[n]
 }
 
+// Catalan number
 func recursiveNumTrees(n int) int {
 	var dfs func(k int) int
 	dfs = func(k int) int {
@@ -26,6 +28,8 @@ func recursiveNumTrees(n int) int {
 			return 1
 		}
 		for i := 0 ; i < k ; i++ {
+			// for given root
+			// result += left side * right side
 			result += dfs(i) * dfs(k - i - 1)
 		}
 		return result
@@ -34,9 +38,9 @@ func recursiveNumTrees(n int) int {
 }
 
 func main() {
-	fmt.Println(recursiveNumTrees(1))
-	fmt.Println(recursiveNumTrees(2))
-	fmt.Println(recursiveNumTrees(3))
-	fmt.Println(recursiveNumTrees(4))
-	fmt.Println(recursiveNumTrees(5))
+	fmt.Println(numTrees(1))
+	fmt.Println(numTrees(2))
+	fmt.Println(numTrees(3))
+	fmt.Println(numTrees(4))
+	fmt.Println(numTrees(5))
 }
