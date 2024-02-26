e819641007d692bd5f15c559a0b559180485987e
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-26 10:27:59 -0800

    linear time greedy

--- blind/12_dp/jump_game.go
+++ blind/12_dp/jump_game.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// greedy
+// greedy - shift the goalpost closer to our start
 func canJump(nums []int) bool {
 	goalpost := len(nums) - 1
 	for i := len(nums) - 1 ; i >= 0 ; i-- {
