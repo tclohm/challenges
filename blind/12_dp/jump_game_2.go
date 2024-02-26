4e94d72fed1fc838ec42ba4e0e24e65f4b982cd6
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-02-26 15:06:55 -0800

    jump 2

--- blind/12_dp/jump_game_2.go
+++ blind/12_dp/jump_game_2.go
@@ -7,9 +7,23 @@ import "fmt"
 // return the min number of jumps to reach nums[n - 1]
 
 func jump(nums []int) int {
-	//var n = len(nums)
-	var min = 1000
-	return min
+	var n = len(nums) - 1
+	var dfs func(position int) int
+	dfs = func(position int) int {
+		var min = 10001
+		if position >= n { return 0 }
+		// why the 1? because the
+		for i := 1 ; i <= nums[position] ; i++ {
+			min = minimum(min, 1 + dfs(i + position))
+		}
+		return min
+	}
+	return dfs(0)
+}
+
+func max(a, b int) int {
+	if a > b { return a }
+	return b
 }
 
 func minimum(a, b int) int {
@@ -20,4 +34,14 @@ func minimum(a, b int) int {
 func main() {
 	fmt.Println(jump([]int{2,3,1,1,4}))
 	fmt.Println(jump([]int{3,2,0,1,4}))
+	fmt.Println(jump([]int{1,1,1,1,1,1,1,1}))
+	fmt.Println(jump([]int{1,2,2,1,1,2,2,1}))
 }
+
+// 2 => [3,1]
+// 		3 => [1,1,4]
+// 		   1 => [1]
+// 		  		1 => [4]
+//
+// 		   1 => [1]
+//				 1 => [4]
