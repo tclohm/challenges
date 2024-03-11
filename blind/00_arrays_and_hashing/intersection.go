d0ec1dcd14210bc559ebab62a83a1dd359e79edd
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-03-10 17:21:59 -0700

    cool beans

--- blind/00_arrays_and_hashing/intersection.go
+++ blind/00_arrays_and_hashing/intersection.go
@@ -3,22 +3,17 @@ package main
 import "fmt"
 
 func intersection(nums1, nums2 []int) []int {
-	ht1 := map[int]bool{}
-	ht2 := map[int]bool{}
-
+	seen := map[int]bool{}
 	result := []int{}
 
 	for _, d := range nums1 {
-		ht1[d] = true
+		seen[d] = true
 	}
 
 	for _, d := range nums2 {
-		ht2[d] = true
-	}
-
-	for k, _ := range ht1 {
-		if _, ok := ht2[k]; ok {
-			result = append(result, k)
+		if seen[d] == true {
+			result = append(result, d)
+			seen[d] = false
 		}
 	}
 
