61479ee5ed1237652fa64a46c9078f1a533b9202
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-03-21 13:17:11 -0700

    first pass

--- blind/05_linked_list/merge_btn_linked_lists.go
+++ blind/05_linked_list/merge_btn_linked_lists.go
@@ -32,9 +32,40 @@ func build(nums []int) *Node {
 	return head
 }
  
-// func mergeInBetween(list1 *Node, a int, b int, list2 *Node) *Node {
-    
-// }
+
+// constraints
+// 3 <= length of list1 <= 10**4
+// 1 <= a <= b < length of list1 - 1
+// 1 <= lenght of list2 <= 10**4
+func mergeInBetween(list1 *Node, a int, b int, list2 *Node) *Node {
+	var pointer1 *Node = list1
+    var pointer2 *Node = list1
+    var head = list1
+    var start = 1
+    var end = 0
+
+    for end <= b {
+    	end++
+    	pointer2 = pointer2.next
+    }
+
+    for start < a {
+    	start++
+    	pointer1 = pointer1.next
+    }
+
+    var current = list2
+
+    for current.next != nil {
+    	current = current.next
+    }
+
+    current.next = pointer2
+
+    pointer1.next = list2
+
+    return head
+}
 
 func main() {
 	first := build([]int{10,1,13,6,9,5})
@@ -43,8 +74,14 @@ func main() {
 	third := build([]int{0,1,2,3,4,5,6})
 	fourth := build([]int{1000000,1000001,1000002,1000003,1000004})
 
+	// first.Print()
+	// second.Print()
+	// third.Print()
+	// fourth.Print()
+
+	mergeInBetween(first, 3, 4, second)
+	mergeInBetween(third, 2, 5, fourth)
+
 	first.Print()
-	second.Print()
 	third.Print()
-	fourth.Print()
 }
