10e8141153acac4980b34a5dda8a5857a5c60ab6
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-04-05 08:55:57 -0700

    fun with stacks

--- blind/03_stack/maxNestingDepthOfTheParentheses.go
+++ blind/03_stack/maxNestingDepthOfTheParentheses.go
@@ -2,8 +2,48 @@ package main
 
 import "fmt"
 
+type stack struct {
+	container []rune
+}
+
+func (s *stack) pop() rune {
+	end := len(s.container) - 1
+	el := s.container[end]
+   	s.container = s.container[:end]
+   	return el
+}
+
+func (s *stack) push(v rune) {
+	s.container = append(s.container, v)
+}
+
+func (s *stack) isEmpty() bool {
+	return len(s.container) == 0
+}
+
+func max(a, b int) int {
+	if a > b { return a }
+	return b
+}
+
 func maxDepth(s string) int {
-    return 1
+	depth := 0
+	deepest := 0
+	stack := &stack{}
+    for _, r := range s {
+    	if !stack.isEmpty() && r == ')'{
+    		if stack.pop() == '(' {
+    			depth--
+    		}
+    	}
+    	if r == '(' {
+    		stack.push(r)
+    		depth++
+    	}
+    	deepest = max(deepest, depth)
+    }
+
+    return deepest
 }
 
 func main() {
