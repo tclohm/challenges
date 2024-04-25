e12a7c7aceaba7e19bc92a3547e698daf94c1a8a
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-04-25 11:33:23 -0700

    working on it

--- blind/18_extra/longestIdealString.go
+++ blind/18_extra/longestIdealString.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"math"
+	"fmt"
+)
+
+func longestIdealString(s string, k int) int {
+	var result int
+	var dfs func(index int, previous string) int
+	dfs = func(index int, previous string) int {
+		if index == len(s) { return 0 }
+
+
+		// skip
+		result = dfs(index + 1, previous)
+
+		
+		if previous != "" {
+			index_number := s[index] - 'a'
+			prev_number := previous - 'a'
+			if int(math.Abs(float64(index_number) - float64(prev_number))) <= k {
+				result = max(result, dfs(index + 1, string(s[index])))
+			}
+		}
+
+		return result
+	}
+
+	return dfs(0, "")
+}
+
 
 func main() {
-	
+	fmt.Println(longestIdealString("acfgbd", 2))
+	fmt.Println(longestIdealString("abcd", 3))
 }
