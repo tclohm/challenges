2c3006137c7cf8aa18ac486692819df9faa16ab9
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-05-16 06:57:20 -0700

    path max gold solution

--- blind/10_graph/path_max_gold.go
+++ blind/10_graph/path_max_gold.go
@@ -4,15 +4,30 @@ import "fmt"
 
 func maxGold(grid [][]int) int {
   ROWS, COLS := len(grid), len(grid[0])
-  visited := make(map[string])
+  visited := make(map[string]bool)
   
-  var dfs func(r, c int)
-  dfs = func(r, c int) {}
+  var dfs func(r, c int, visited map[string]bool) int
+  dfs = func(r, c int, visited map[string]bool) int {
+    strRC := fmt.Sprintf("%d %d", r, c)
+    _, exist := visited[strRC]
+    if min(r, c) < 0 || r == ROWS || c == COLS || grid[r][c] == 0 || exist {
+      return 0
+    }
 
+    visited[strRC] = true
+    result := grid[r][c]
+
+    for _, neighbor := range [][]int{{r + 1, c}, {r - 1, c}, {r, c + 1}, {r, c - 1}} {
+      nR, nC := neighbor[0], neighbor[1]
+      result = max(result, grid[r][c] + dfs(nR, nC, visited))
+    }
+    delete(visited, strRC)
+    return result
+  }
   result := 0
-  for r in range ROWS {
-    for c in range COLS {
-      dfs(r, c)
+  for r := range ROWS {
+    for c := range COLS {
+      result = max(result, dfs(r, c, visited))
     }
   }
   return result
