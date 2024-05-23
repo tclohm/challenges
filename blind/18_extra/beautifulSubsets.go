1b8f227eee2d33eb4ae500326d25abcc429865c1
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-05-23 07:16:55 -0700

    starting beautiful subsets

---
+++ blind/18_extra/beautifulSubsets.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func beautifulSubsets(nums []int, k int) int {
+  return 0
+}
+
+func main() {
+  fmt.Println(beautifulSubsets([]int{2,4,6}, 2))
+  fmt.Println(beautifulSubsets([]int{1}, 1))
+}
