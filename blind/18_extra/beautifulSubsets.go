015dda6fe1922ba0d8c61f9fbf1ff5891297374d
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-05-24 12:30:03 -0700

    didnt finish the op solution

--- blind/18_extra/beautifulSubsets.go
+++ blind/18_extra/beautifulSubsets.go
@@ -26,6 +26,55 @@ func beautifulSubsets(nums []int, k int) int {
   return dfs(0, map[int]int{}) - 1
 }
 
+func optimalSolution(nums []int, k int) int {
+  counter := make(map[int]int)
+
+  for _, n := range nums {
+    counter[n] += 1
+  }
+
+  groups := []map[int]int{}
+  visited := make(map[int]bool)
+  
+
+  var helper func(n int, g map[int]int) int
+  helper = func(n int, g map[int]int) int {
+    if _, in := g[n]; !in { return 1 }
+    skip := helper(n + k, g)
+    include := helper(n + 2*k, g)
+    return skip + include
+  }
+
+  for key, _ := range counter {
+    if _, in := visited[key]; in { continue }
+    g := make(map[int]int)
+
+    for counter[key - k] > 0 {
+      key -= key
+    }
+
+    for counter[key] > 0 {
+      g[key] = counter[key]
+      visited[key] = true
+    }
+
+    groups = append(groups, g)
+  }
+
+  result := 1
+  // variation of house robber algo
+  for g := range groups {
+    n := 10000000
+    for key := range g {
+      if key < n {
+        n = key
+      }
+    }
+    result *= helper(n, g)
+  }
+  return result - 1
+}
+
 func main() {
   fmt.Println(beautifulSubsets([]int{2,4,6}, 2))
   fmt.Println(beautifulSubsets([]int{1}, 1))
