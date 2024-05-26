eafcf9a06c224122aac7038cf57ffef57ca31df7
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-05-26 13:49:05 -0700

    word break

--- blind/09_backtracking/word_break_2.go
+++ blind/09_backtracking/word_break_2.go
@@ -1,11 +1,42 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "strings"
+)
 // Time : 2^n, Space : n
 func wordBreak(s string, dict []string) []string {
+  var set = createSet(dict)
+  var current = []string{}
+  var result = []string{}
+
   var backtracking func (index int)
-  backtracking = func (index int) {}
-  return []string{}
+  backtracking = func (index int) {
+    if index == len(s) {
+      result = append(result, strings.Join(current, " "))
+      return
+    }
+    for jndex := index ; jndex < len(s) ; jndex++ {
+      word := s[index:jndex+1]
+      if _, exist := set[word]; exist {
+        current = append(current, word)
+        backtracking(jndex + 1)
+        // pop
+        current = current[:len(current) - 1]
+      }
+    }
+  }
+
+  backtracking(0)
+  return result
+}
+
+func createSet(words []string) map[string]bool {
+  res := map[string]bool{}
+  for _, w := range words {
+    res[w] = true
+  }
+  return res
 }
 
 func main() {
