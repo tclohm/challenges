3abdb174702cfe971dbe9584a1b1899d4c3322b3
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-06-17 16:21:04 -0700

    more efficent in time but not space

--- blind/18_extra/minNumSeats.go
+++ blind/18_extra/minNumSeats.go
@@ -20,9 +20,58 @@ func minMoveSeats(seats, students []int) int {
 	return count
 }
 
+func eff(students, seats []int) int {
+	maxIndex := max(maxIn(students), maxIn(seats)) + 1
+	countSeats := make([]int, maxIndex, maxIndex)
+	countStudents := make([]int, maxIndex, maxIndex)
+
+	for _, seat := range seats {
+		countSeats[seat] += 1
+	}
+	for _, student := range students {
+		countStudents[student] += 1
+	}
+
+	i, j := 0, 0
+	res := 0
+	remain := len(students)
+	for remain > 0 {
+		if countSeats[i] == 0 {
+			i += 1
+		}
+		if countStudents[j] == 0 {
+			j += 1
+		}
+		if countSeats[i] > 0 && countStudents[j] > 0 {
+			res += int(math.Abs(float64(i - j)))
+			countSeats[i] -= 1
+			countStudents[j] -= 1
+			remain -= 1
+		}
+	}
+	return res
+}
+
+func maxIn(nums []int) int {
+	big := -1
+	for _, n := range nums {
+		if n > big {
+			big = n
+		}
+	}
+	return big
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
 func main() {
-	fmt.Println(minMoveSeats([]int{3,1,5}, []int{2,7,4}))
-	fmt.Println(minMoveSeats([]int{4,1,5,9}, []int{1,3,2,6}))
-	fmt.Println(minMoveSeats([]int{2,2,6,6}, []int{1,3,2,6}))
+	fmt.Println(eff([]int{3,1,5}, []int{2,7,4}))
+	fmt.Println(eff([]int{4,1,5,9}, []int{1,3,2,6}))
+	fmt.Println(eff([]int{2,2,6,6}, []int{1,3,2,6}))
 
 }
