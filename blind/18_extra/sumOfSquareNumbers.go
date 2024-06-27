51972843479d1f817711331fd6143acb0db6fd7a
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-06-26 23:11:11 -0700

    two sum

--- blind/18_extra/sumOfSquareNumbers.go
+++ blind/18_extra/sumOfSquareNumbers.go
@@ -1,14 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func isSumOfSquareNumbers(result int) bool {
+func bf(result int) bool {
 	for i := 1 ; i < result ; i++ {
 		for j := 1 ; j < result ; j++ {
 			if i * i + j * j == result {
 				return true
 			}
  		}
+	}
+	return false
+}
+
+func isSumOfSquareNumbers(result int) bool {
+	squareRoot := map[int]bool{}
+	for i := 1 ; i < int(math.Floor(math.Sqrt(float64(result)))) ; i++ {
+		squareRoot[i * i] = true
+	}
+
+	a := 0
+	for a * a <= result {
+		target := result - a * a
+		if _, ok := squareRoot[target]; ok {
+			return true
+		}
+		a++
 	}
 	return false
 }
