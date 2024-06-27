f9ed818793c204303edaba975d2a801732948c24
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-06-27 14:55:08 -0700

    cool doesnt work

--- blind/18_extra/countNumberOfNiceSubarrays.go
+++ blind/18_extra/countNumberOfNiceSubarrays.go
@@ -12,7 +12,7 @@ import "fmt"
 // - add the number of nice subarrays to result
 func numberOfSubarrays(nums []int, k int) int {
 	count := 0
-	left, middle := 0, 0
+	left, middle, odd := 0, 0, 0
 	for right, _ := range nums {
 		if nums[right] % 2 != 0 {
 			odd++
@@ -30,13 +30,13 @@ func numberOfSubarrays(nums []int, k int) int {
 			for nums[middle] % 2 != 0 {
 				middle++
 			}
-			count += (m - l) + 1
+			count += (middle - left) + 1
 		}
 	}
 	return count
 }
 
 func main() {
-	fmt.Println(numberOfSubarray([]int{1,1,2,1,1}, 3))
-	fmt.Println(numberOfSubarray([]int{2,4,6}, 1))
+	fmt.Println(numberOfSubarrays([]int{1,1,2,1,1}, 3))
+	fmt.Println(numberOfSubarrays([]int{2,4,6}, 1))
 }
