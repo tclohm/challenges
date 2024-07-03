669129c9e3bd5af08ccc28bac270251e2f24cf3b
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-02 17:06:09 -0700

    space complexity down

--- blind/18_extra/bitFlips.go
+++ blind/18_extra/bitFlips.go
@@ -4,26 +4,24 @@ import "fmt"
 
 func minK(nums []int, k int) int {
 	var (
-		queue = []int{}
+		currentWindowFlips = 0
 		res = 0
 	)
 
 	for i, _ := range nums {
-
-		for len(queue) > 0 && queue[0] + k - 1 == 0 {
-			queue = queue[1:]
+		if i - k >= 0 && nums[i - k] == 2 {
+			currentWindowFlips -= 1
 		}
 
-
-		if nums[i] + len(queue) % 2 == 0 {
+		if nums[i] + currentWindowFlips % 2 == 0 {
 			if i + k > len(nums) { 
-				return -1 
+				return -1
 			}
 			res += 1
-			queue = append(queue, i)
+			currentWindowFlips += 1
+			nums[i] = 2
 		}
 	}
-
 	return res
 }
 
