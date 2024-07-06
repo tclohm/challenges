7a54cdcc054f5aa80eee9ec28a334e729cbd578d
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-06 15:03:51 -0700

    it works but the 123 has dups

--- blind/18_extra/permute.go
+++ blind/18_extra/permute.go
@@ -122,22 +122,20 @@ func permutationUsingInsertion(nums []int) [][]int {
 		return [][]int{nums}
 	}
 
-	result := [][]int{}
+	var result [][]int
 
 	for i, num := range nums {
-
-		rest := nums[:i]
-		rest = append(rest, nums[i + 1:]...)
-
+		rest := append([]int{}, nums[:i]...)
+		rest = append(rest, nums[i+1:]...)
+		
 		for _, perm := range permutationUsingInsertion(rest) {
-
-			for j := range len(perm) {
-				newPerm := perm[:j]
-				newPerm = append(newPerm, num)
-				newPerm = append(newPerm, perm[j:]...)
+			for j := 0 ; j < len(perm) ; j++ {
+				newPerm := make([]int, len(perm)+1)
+				copy(newPerm[:j], perm[:j])
+				newPerm[j] = num
+				copy(newPerm[j + 1:], perm[j:])
 				result = append(result, newPerm)
 			}
-
 		} 
 	}
 	return result
