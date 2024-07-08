b007ccd7f77b3dd7307b266e2b1fc21bcf059a73
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-08 09:02:23 -0700

    math

--- blind/18_extra/waterbottle.go
+++ blind/18_extra/waterbottle.go
@@ -5,11 +5,12 @@ import "fmt"
 // exchange empty water bottles for full ones
 // return the max number of water bottles we can drink
 func howMuch(have int, exchangeRate int) int {
-	total := have
-	for i := 1 ; i < total ; i += exchangeRate {
-		total++
-	}
-	return total
+	// total := have
+	// for i := 1 ; i < total ; i += exchangeRate {
+	// 	total++
+	// }
+	// return total
+	return have + (have - 1) / (exchangeRate - 1)
 }
 
 func main() {
