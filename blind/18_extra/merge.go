0cdde9d051ff02cfe96b8f5e94c16e629a175b94
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-09 13:54:14 -0700

    first pass

--- blind/18_extra/merge.go
+++ blind/18_extra/merge.go
@@ -35,11 +35,39 @@ func printLinkedList(head *Node) {
 	fmt.Println(buffer.String())
 }
 
+func isZero(head *Node) bool {
+	if head.val == 0 {
+		return true
+	}
+	return false
+}
+
 func mergeNodesBetweenZero(head *Node) []int {
- return []int{0,0}
+	var result = []int{}
+	for head != nil {
+		fmt.Println(head.val)
+		var adding int = 0
+		if nil != head && isZero(head) {
+			head = head.next
+			for nil != head && head.val != 0 {
+				adding += head.val
+				head = head.next
+			}
+		}
+		if adding > 0 {
+			result = append(result, adding)
+			adding = 0
+		}
+		if head == nil {
+			return result
+		}
+		head = head.next
+	}
+	return result
 }
 
 func main() {
 	one := buildLinkedList([]int{0,3,1,0,4,5,2,0})
 	printLinkedList(one)
+	fmt.Println(mergeNodesBetweenZero(one))
 }
