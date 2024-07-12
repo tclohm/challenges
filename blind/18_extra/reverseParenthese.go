624a71dfde90064cc7d77e7039cde29bc25dcbd7
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-12 10:14:42 -0700

    throwing some errors on

--- blind/18_extra/reverseParenthese.go
+++ blind/18_extra/reverseParenthese.go
@@ -36,3 +36,36 @@ func main() {
 	fmt.Println(reverse("(u(love)i)"))
   fmt.Println(reverse("(ed(et(oc))el)"))
 }
+
+func rev(s string) string {
+	pair := map[int]int
+	stack := []string{}
+
+	for i, r := range s {
+		c := string(r)
+		if c == "(" {
+			stack = append(stack, c)
+		} else if c == ")" {
+			last := len(stack) - 1
+			el := stack[last]
+			stack = stack[:last]
+			pair[i] = el
+			pair[el] = j
+		}
+	}
+
+	index, direction := 0, 1
+	result := []string{}
+
+	for index < len(s) {
+		if s[index] == "(" || s[index] == ")" {
+			index = pair[index]
+			direction = -direction
+		} else {
+			result = append(result, s[i])
+		}
+		i += direction
+	}
+
+	return strings.Join(result, "")
+}
