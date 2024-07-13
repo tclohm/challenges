788ebbabbd4ac31122c4ac360e578c3ffb567637
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-12 17:16:57 -0700

    starting max score

---
+++ blind/18_extra/maxScore.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// remove "ab" receives x points
+// remove "ba" receives y points
+
+func maxScore(s string, x, y int) int {
+	return 1
+}
+
+func main() {
+	fmt.Println(maxScore("cdbcbbaaabab", 4, 5))
+	fmt.Println(maxScore("aabbaaxybbaabb", 5, 4))
+}
