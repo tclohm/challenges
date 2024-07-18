6aba3c97435655d30fe683d4bd49ed603af79b72
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-18 06:59:19 -0700

    shrug

--- blind/18_extra/deleteNode.go
+++ blind/18_extra/deleteNode.go
@@ -9,15 +9,23 @@ type Node struct {
 }
 
 func buildTree(nums []int) *Node {
-	for i, n := range nums {
-		node := Node{}
+	var root *Node
+	var nodes = map[*Node]
+	for i := 0 ; i < len(nums) ; i++ {
+		node := Node{val: nums[i]}
+		if i + 1 < len(nums) {
+			node.left = &Node{val: nums[i + 1]}
+		}
+
+		if i + 2 < len(nums) {
+			node.right = &Node{val: nums[i + 2]}
+		}
 	}
 }
-
 func deleteNodes(root *Node, toDelete []int) []*Node {
 	return []int{&Node{val: 0}}
 }
 
 func main() {
-	fmt.Println(deleteNodes())
+	fmt.Println(buildTree([]int{1,2,3,4,5,6,7,8}))
 }
