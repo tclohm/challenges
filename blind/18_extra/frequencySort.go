246ac648aa792940afc068cefd8c3a12ed337a89
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-24 16:50:12 -0700

    hash table and sort

--- blind/18_extra/frequencySort.go
+++ blind/18_extra/frequencySort.go
@@ -1,15 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func frequency(nums []int) []int {
-	freq := map[int]int{}
+	ht := map[int]int{}
+
 	for _, n := range nums {
-		freq[n]++
+		ht[n]++
 	}
 
+	sort.Slice(nums, func(i, j int) bool {
+		if ht[nums[i]] == ht[nums[j]] {
+			return nums[i] > nums[j]
+		}
+		return ht[nums[i]] < ht[nums[j]]
+	})
 
-	return []int{} 
+	return nums
 }
 
 
@@ -17,4 +27,5 @@ func main() {
 	fmt.Println(frequency([]int{1,1,2,2,2,3}))
 	fmt.Println(frequency([]int{2,3,1,3,2}))
 	fmt.Println(frequency([]int{-1,1,-6,4,5,-6,1,4,1}))
+
 }
