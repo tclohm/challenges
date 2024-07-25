549d144dbf90e5fbbe5634e516c5270eba6ac67c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-24 20:38:09 -0700

    start the sort jumbled

---
+++ blind/18_extra/sortJumbled.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func sortJumbled(mapping, nums []int) []int {
+	return []int{}
+}
+
+func main() {
+	fmt.Println(sortJumbled([]int{8,9,4,0,2,1,3,5,7,6}, []int{991,338,38}))
+	fmt.Println(sortJumbled([]int{0,1,2,3,4,5,6,7,8,9}, []int{789,456,123}))
+}
