0612fd5828be47a7130fc6ddca600e0de283484b
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-28 14:46:22 -0700

    ok

--- toitoi/00ArrayAndHash/productExpectSelf.go
+++ toitoi/00ArrayAndHash/productExpectSelf.go
@@ -4,12 +4,18 @@ import "fmt"
 
 func productExceptSelf(nums []int) []int {
 	result := make([]int, len(nums), len(nums))
-	total := 1
-	result[0] = nums[0]	
+	result[0] = nums[0]
 	for i := 1 ; i < len(nums) ; i++ {
-		result[i] = nums[i]
-		total = nums[i] * total
+		result[i] = 1
 	}
+	
+	for i := 0 ; i < len(result) ; i++ {
+		for j := 0 ; j < len(result) ; j++ {
+			if j == i { continue }
+			result[i] *= nums[j]
+		}
+	}
+
 
 	return result
 }
