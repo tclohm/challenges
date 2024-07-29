0b0c13fd9ec5fb707827b9cc7e156d31adb54b14
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-29 08:17:52 -0700

    fucking postfix

--- toitoi/00ArrayAndHash/productExpectSelf.go
+++ toitoi/00ArrayAndHash/productExpectSelf.go
@@ -4,18 +4,20 @@ import "fmt"
 
 func productExceptSelf(nums []int) []int {
 	result := make([]int, len(nums), len(nums))
-	result[0] = nums[0]
-	for i := 1 ; i < len(nums) ; i++ {
+	for i := 0 ; i < len(result) ; i++ {
 		result[i] = 1
 	}
-	
-	for i := 0 ; i < len(result) ; i++ {
-		for j := 0 ; j < len(result) ; j++ {
-			if j == i { continue }
-			result[i] *= nums[j]
-		}
+
+	for i := 1 ; i < len(nums) ; i++  {
+		result[i] = result[i - 1] * nums[i - 1]
 	}
 
+	postfix := 1
+
+	for i := len(result) - 1 ; i >= 0 ; i-- {
+		result[i] *= postfix
+		postfix *= nums[i]
+	}
 
 	return result
 }
