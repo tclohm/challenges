17c3cc241170a286f5defdb3a45c169abd5ae12c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-30 19:28:32 -0700

    great

--- toitoi/00ArrayAndHash/minDeletion.go
+++ toitoi/00ArrayAndHash/minDeletion.go
@@ -5,13 +5,31 @@ import (
 )
 
 func minDeletion(s string) int {
-	aCountRight := make([]int, len(s), len(s))
-	for i := len(s) - 2 ; i >= 0 ; i-- {
-		
+	aCountRight := 0
+	for _, c := range s {
+		if c == 'a' {
+			aCountRight += 1
+		}
 	}
-	return 1
+
+	bCountLeft := 0
+	result := len(s)
+
+	for _, c := range s {
+		if c == 'a' {
+			aCountRight -= 1
+		}
+
+		result = min(result, bCountLeft + aCountRight)
+
+		if c == 'b' {
+			bCountLeft += 1
+		}
+	}
+	return result
 }
 
 func main(){
-	
+	fmt.Println(minDeletion("aababbab"))
+	fmt.Println(minDeletion("aababbab"))
 }
