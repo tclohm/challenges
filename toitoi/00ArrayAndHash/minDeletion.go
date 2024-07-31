3e5c72825060bb6697c13b8ea05f97c661970bb6
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-07-30 19:16:01 -0700

    min delete

---
+++ toitoi/00ArrayAndHash/minDeletion.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func minDeletion(s string) int {
+	aCountRight := make([]int, len(s), len(s))
+	for i := len(s) - 2 ; i >= 0 ; i-- {
+		
+	}
+	return 1
+}
+
+func main(){
+	
+}
