5516e84221a58a109f395aa7044ba68e9556302c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-08-03 11:21:50 -0700

    min swap is done

---
+++ toitoi/01TwoPounters/minSwap.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func count(nums []int, k int) int {
+	var t int = 0
+	for _, n := range nums {
+		if n == k {
+			t++
+		}
+	}
+	return t
+}
+
+func minSwap(nums []int) int {
+	var (
+		N = len(nums)
+		totalOnes = count(nums, 1)
+		windowOnes = 0
+		maxWindow = 0
+		left = 0
+	)
+	for right := range 2 * N {
+		if nums[right % N] == 1 {
+			windowOnes += 1
+		}
+		if right - left + 1 > totalOnes {
+			windowOnes -= nums[left % N]
+			left += 1
+		}
+		maxWindow = max(maxWindow, windowOnes)
+	}
+
+	return totalOnes - maxWindow
+}
+
+func main() {
+	fmt.Println(minSwap([]int{0,1,0,1,1,0,0}))
+	fmt.Println(minSwap([]int{0,1,1,1,0,0,1,1,0}))
+}
