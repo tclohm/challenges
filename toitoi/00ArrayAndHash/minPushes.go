e02ba7c5cd3205b3a8b5936ac36f624d24a89c02
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-08-06 19:53:06 -0700

    array

--- toitoi/00ArrayAndHash/minPushes.go
+++ toitoi/00ArrayAndHash/minPushes.go
@@ -1,9 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"math"
+)
 
 func minPushes(word string) int {
-	return 1
+	counts := make([]rune, 26, 26)
+	for _, r := range word {
+		counts[r - 'a'] += 1
+	}
+
+	sort.Slice(counts, func(i, j int) bool {
+		return counts[i] > counts[j]
+	})
+
+	res := 0
+	distinct := 0
+	for _, n := range counts {
+		res += int(n) * int(math.Floor(float64(1 + distinct / 8)))
+		distinct += 1
+	}
+
+	return res 
 }
 
 func main() {
