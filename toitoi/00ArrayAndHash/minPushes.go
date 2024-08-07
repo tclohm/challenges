802aaf5047664f7b45d2bfd4e1fa4921d48b4b1d
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-08-06 19:32:07 -0700

    min pushes

---
+++ toitoi/00ArrayAndHash/minPushes.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func minPushes(word string) int {
+	return 1
+}
+
+func main() {
+	fmt.Println(minPushes("abcde"))
+	fmt.Println(minPushes("xyzxyzxyzxyz"))
+	fmt.Println(minPushes("aabbccddeeffgghhiiiiii"))
+}
